c3500f8f7fadd16a2ea93955b3101ed0291c1382
Ben Christensen <benjchristensen@netflix.com>
2013-01-10 14:45:59 -0800

    Add more arities

---
+++ rxjava-core/src/main/java/org/rx/functions/Func5.java
@@ -0,0 +1,5 @@
+package org.rx.functions;
+
+public interface Func5<R, T1, T2, T3, T4, T5> {
+    public R call(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5);
+}
---
+++ rxjava-core/src/main/java/org/rx/functions/Func8.java
@@ -0,0 +1,5 @@
+package org.rx.functions;
+
+public interface Func8<R, T1, T2, T3, T4, T5, T6, T7, T8> {
+    public R call(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8);
+}
