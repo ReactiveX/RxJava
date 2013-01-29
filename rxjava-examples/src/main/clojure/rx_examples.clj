619228cd2a8656f7fef021a4f364011b7ef6a564
Ben Christensen <benjchristensen@netflix.com>
2013-01-29 13:40:15 -0800

    example code as part of "How to Use"

--- rxjava-examples/src/main/clojure/rx_examples.clj
+++
@@ -1,8 +0,0 @@
-(ns rx-examples
-  (:use clojure.test)
-  (import rx.observables.Observable))
-
-(defn hello
-  [&rest]
-  (-> (Observable/toObservable &rest)
-    (.subscribe #(println (str "Hello " % "!")))))
