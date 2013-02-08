a5bf8babca9f7664df90ebcc0ee5ff1b489016a2
Ben Christensen <benjchristensen@netflix.com>
2013-02-07 22:45:24 -0800

    clojure example code
    - examples are in /src/examples/clojure
    - placeholder for unit tests in /src/tests

---
+++ language-adaptors/rxjava-clojure/src/test/clojure/rx/lang/clojure/observable-tests.clj
@@ -0,0 +1,5 @@
+(ns rx.lang.clojure.examples.rx-examples
+  (import rx.Observable))
+
+;; still need to get this wired up in build.gradle to run as tests
+; (-> (rx.Observable/toObservable [\"one\" \"two\" \"three\"]) (.take 2) (.subscribe (fn [arg] (println arg))))
