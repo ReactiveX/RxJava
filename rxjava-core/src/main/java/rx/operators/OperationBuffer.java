3ed9f0f35aa82e04798ff4f35a32207787d78074
Michael de Jong <michaelj@minicom.nl>
2013-05-30 00:02:02 +0200

    Added license header to OperationBuffer.

--- rxjava-core/src/main/java/rx/operators/OperationBuffer.java
+++ rxjava-core/src/main/java/rx/operators/OperationBuffer.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rx.operators;
 
 import java.util.ArrayList;
@@ -925,8 +940,8 @@ public final class OperationBuffer {
             Observable<BufferOpening> openings = Observable.create(new Func1<Observer<BufferOpening>, Subscription>() {
                 @Override
                 public Subscription call(Observer<BufferOpening> observer) {
-                    push(observer, new BufferOpenings(), 50);
-                    push(observer, new BufferOpenings(), 200);
+                    push(observer, BufferOpenings.create(), 50);
+                    push(observer, BufferOpenings.create(), 200);
                     complete(observer, 250);
                     return Subscriptions.empty();
                 }
@@ -938,7 +953,7 @@ public final class OperationBuffer {
                     return Observable.create(new Func1<Observer<BufferClosing>, Subscription>() {
                         @Override
                         public Subscription call(Observer<BufferClosing> observer) {
-                            push(observer, new BufferClosings(), 100);
+                            push(observer, BufferClosings.create(), 100);
                             complete(observer, 101);
                             return Subscriptions.empty();
                         }
@@ -978,7 +993,7 @@ public final class OperationBuffer {
                     return Observable.create(new Func1<Observer<BufferClosing>, Subscription>() {
                         @Override
                         public Subscription call(Observer<BufferClosing> observer) {
-                            push(observer, new BufferClosings(), 100);
+                            push(observer, BufferClosings.create(), 100);
                             complete(observer, 101);
                             return Subscriptions.empty();
                         }
