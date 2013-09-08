367eb224ac7322f4df1099cd4aa446666c569ccf
jmhofer <jmhofer.github@johoop.de>
2013-09-08 13:33:50 +0200

    removed unused imports

--- rxjava-core/src/main/java/rx/Observable.java
+++ rxjava-core/src/main/java/rx/Observable.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -67,7 +66,6 @@ import rx.operators.OperationWindow;
 import rx.operators.OperationZip;
 import rx.operators.SafeObservableSubscription;
 import rx.operators.SafeObserver;
-import rx.operators.OperationInterval.Interval;
 import rx.plugins.RxJavaErrorHandler;
 import rx.plugins.RxJavaObservableExecutionHook;
 import rx.plugins.RxJavaPlugins;
