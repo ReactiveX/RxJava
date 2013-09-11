0603e395b680655a923e138582d779c65d001651
samuelgruetter <samuel.gruetter@epfl.ch>
2013-09-11 10:55:03 +0200

    Java example using Scala Observable code

---
+++ language-adaptors/rxjava-scala/src/main/scala/rx/lang/scalatests/MovieLib.scala
@@ -0,0 +1,15 @@
+package rx.lang.scalatests
+
+import rx.lang.scala.Observable
+
+class Movie(val lengthInSeconds: Int) { }
+
+class MovieLib(val moviesStream: Observable[Movie]) {
+  
+  val threshold = 1200
+  
+  def shortMovies: Observable[Movie] = moviesStream.filter(_.lengthInSeconds <= threshold)
+  
+  def longMovies: Observable[Movie] = moviesStream.filter(_.lengthInSeconds > threshold)
+
+}
