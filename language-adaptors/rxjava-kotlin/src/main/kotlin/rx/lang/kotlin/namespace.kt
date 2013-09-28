201f0537b7b75468142f0dbbec13226eb9909241
MarioAriasC <mario@marioarias.org>
2013-09-28 02:15:41 +0100

    Basic Kotlin tests completed

--- language-adaptors/rxjava-kotlin/src/main/kotlin/rx/lang/kotlin/namespace.kt
+++ language-adaptors/rxjava-kotlin/src/main/kotlin/rx/lang/kotlin/namespace.kt
@@ -5,7 +5,11 @@ import rx.Observer
 import rx.Observable
 
 public fun<T> Function1<Observer<in T>, Subscription>.asObservable(): Observable<T> {
-    return Observable.create{ this(it!!) }!!
+    return Observable.create { this(it!!) }!!
+}
+
+public fun<T> Function0<Observable<out T>>.defer(): Observable<T> {
+    return Observable.defer(this)!!
 }
 
 public fun<T> Iterable<T>.asObservable(): Observable<T> {
@@ -14,4 +18,32 @@ public fun<T> Iterable<T>.asObservable(): Observable<T> {
 
 public fun<T> T.asObservable(): Observable<T> {
     return Observable.from(this)!!
+}
+
+public fun<T> Throwable.asObservable(): Observable<T> {
+    return Observable.error(this)!!
+}
+
+public fun<T> Pair<T, T>.asObservable(): Observable<T> {
+    return Observable.from(this.component1(), this.component2())!!
+}
+
+public fun<T> Triple<T, T, T>.asObservable(): Observable<T> {
+    return Observable.from(this.component1(), this.component2(), this.component3())!!
+}
+
+public fun<T> Pair<Observable<T>, Observable<T>>.merge(): Observable<T> {
+    return Observable.merge(this.component1(), this.component2())!!
+}
+
+public fun<T> Triple<Observable<T>, Observable<T>, Observable<T>>.merge(): Observable<T> {
+    return Observable.merge(this.component1(), this.component2(), this.component3())!!
+}
+
+public fun<T> Pair<Observable<T>, Observable<T>>.mergeDelayError(): Observable<T> {
+    return Observable.mergeDelayError(this.component1(), this.component2())!!
+}
+
+public fun<T> Triple<Observable<T>, Observable<T>, Observable<T>>.mergeDelayError(): Observable<T> {
+    return Observable.mergeDelayError(this.component1(), this.component2(), this.component3())!!
 }
--- language-adaptors/rxjava-kotlin/src/test/kotlin/rx/lang/kotlin/BasicKotlinTests.kt
+++ language-adaptors/rxjava-kotlin/src/test/kotlin/rx/lang/kotlin/BasicKotlinTests.kt
@@ -11,6 +11,8 @@ import org.mockito.Matchers.*
 import rx.Observer
 import org.junit.Assert.*
 import rx.Notification
+import rx.Subscription
+import kotlin.concurrent.thread
 
 public class BasicKotlinTests {
 
@@ -20,6 +22,10 @@ public class BasicKotlinTests {
     [Before]
     public fun before() {
         MockitoAnnotations.initMocks(this)
+    }
+
+    fun received<T>(): (T?) -> Unit {
+        return {(result: T?) -> a!!.received(result) }
     }
 
     [Test]
@@ -34,6 +40,7 @@ public class BasicKotlinTests {
 
         verify(a, times(1))!!.received("Hello")
     }
+
     [Test]
     public fun testCreateEx() {
 
@@ -50,9 +57,7 @@ public class BasicKotlinTests {
 
     [Test]
     public fun testFilter() {
-        Observable.from(1, 2, 3)!!.filter { it!! >= 2 }!!.subscribe { result ->
-            a!!.received(result)
-        }
+        Observable.from(1, 2, 3)!!.filter { it!! >= 2 }!!.subscribe(received())
         verify(a, times(0))!!.received(1);
         verify(a, times(1))!!.received(2);
         verify(a, times(1))!!.received(3);
@@ -60,9 +65,7 @@ public class BasicKotlinTests {
 
     [Test]
     public fun testFilterEx() {
-        listOf(1, 2, 3).asObservable().filter { it!! >= 2 }!!.subscribe { result ->
-            a!!.received(result)
-        }
+        listOf(1, 2, 3).asObservable().filter { it!! >= 2 }!!.subscribe(received())
         verify(a, times(0))!!.received(1);
         verify(a, times(1))!!.received(2);
         verify(a, times(1))!!.received(3);
@@ -90,19 +93,19 @@ public class BasicKotlinTests {
 
     [Test]
     public fun testMap1() {
-        Observable.from(1)!!.map { v -> "hello_$v" }!!.subscribe { result -> a!!.received(result) }
+        Observable.from(1)!!.map { v -> "hello_$v" }!!.subscribe(received())
         verify(a, times(1))!!.received("hello_1")
     }
 
     [Test]
     public fun testMap1Ex() {
-        1.asObservable().map { v -> "hello_$v" }!!.subscribe { result -> a!!.received(result) }
+        1.asObservable().map { v -> "hello_$v" }!!.subscribe((received()))
         verify(a, times(1))!!.received("hello_1")
     }
 
     [Test]
     public fun testMap2() {
-        Observable.from(1, 2, 3)!!.map { v -> "hello_$v" }!!.subscribe { result -> a!!.received(result) }
+        Observable.from(1, 2, 3)!!.map { v -> "hello_$v" }!!.subscribe((received()))
         verify(a, times(1))!!.received("hello_1")
         verify(a, times(1))!!.received("hello_2")
         verify(a, times(1))!!.received("hello_3")
@@ -110,7 +113,7 @@ public class BasicKotlinTests {
 
     [Test]
     public fun testMap2Ex() {
-        listOf(1, 2, 3).asObservable().map { v -> "hello_$v" }!!.subscribe { result -> a!!.received(result) }
+        listOf(1, 2, 3).asObservable().map { v -> "hello_$v" }!!.subscribe((received()))
         verify(a, times(1))!!.received("hello_1")
         verify(a, times(1))!!.received("hello_2")
         verify(a, times(1))!!.received("hello_3")
@@ -118,21 +121,401 @@ public class BasicKotlinTests {
 
     [Test]
     public fun testMaterialize() {
-        Observable.from(1, 2, 3)!!.materialize()!!.subscribe { result -> a!!.received(result) }
+        Observable.from(1, 2, 3)!!.materialize()!!.subscribe((received()))
         verify(a, times(4))!!.received(any(javaClass<Notification<Int>>()))
         verify(a, times(0))!!.error(any(javaClass<Exception>()))
     }
 
     [Test]
     public fun testMaterializeEx() {
-        listOf(1, 2, 3).asObservable().materialize()!!.subscribe { result -> a!!.received(result) }
+        listOf(1, 2, 3).asObservable().materialize()!!.subscribe((received()))
         verify(a, times(4))!!.received(any(javaClass<Notification<Int>>()))
         verify(a, times(0))!!.error(any(javaClass<Exception>()))
     }
 
+    [Test]
+    public fun testMergeDelayError() {
+        Observable.mergeDelayError(
+                Observable.from(1, 2, 3),
+                Observable.merge(
+                        Observable.from(6),
+                        Observable.error(NullPointerException()),
+                        Observable.from(7)
+                ),
+                Observable.from(4, 5)
+        )!!.subscribe(received(), { e -> a!!.error(e) })
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(1))!!.received(3)
+        verify(a, times(1))!!.received(4)
+        verify(a, times(1))!!.received(5)
+        verify(a, times(1))!!.received(6)
+        verify(a, times(0))!!.received(7)
+        verify(a, times(1))!!.error(any(javaClass<NullPointerException>()))
+    }
+
+    [Test]
+    public fun testMergeDelayErrorEx() {
+
+        Triple(listOf(1, 2, 3).asObservable(),
+                Triple(6.asObservable(),
+                        NullPointerException().asObservable<Int>(),
+                        7.asObservable()
+                ).merge(),
+                listOf(4, 5).asObservable()
+        ).mergeDelayError().subscribe(received(), { e -> a!!.error(e) })
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(1))!!.received(3)
+        verify(a, times(1))!!.received(4)
+        verify(a, times(1))!!.received(5)
+        verify(a, times(1))!!.received(6)
+        verify(a, times(0))!!.received(7)
+        verify(a, times(1))!!.error(any(javaClass<NullPointerException>()))
+    }
+
+    [Test]
+    public fun testMerge() {
+        Observable.merge(
+                Observable.from(1, 2, 3),
+                Observable.merge(
+                        Observable.from(6),
+                        Observable.error(NullPointerException()),
+                        Observable.from(7)
+                ),
+                Observable.from(4, 5)
+        )!!.subscribe(received(), { e -> a!!.error(e) })
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(1))!!.received(3)
+        verify(a, times(0))!!.received(4)
+        verify(a, times(0))!!.received(5)
+        verify(a, times(1))!!.received(6)
+        verify(a, times(0))!!.received(7)
+        verify(a, times(1))!!.error(any(javaClass<NullPointerException>()))
+    }
+
+    [Test]
+    public fun testMergeEx() {
+        Triple(listOf(1, 2, 3).asObservable(),
+                Triple(6.asObservable(),
+                        NullPointerException().asObservable<Int>(),
+                        7.asObservable()
+                ).merge(),
+                listOf(4, 5).asObservable()
+        ).merge().subscribe(received(), { e -> a!!.error(e) })
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(1))!!.received(3)
+        verify(a, times(0))!!.received(4)
+        verify(a, times(0))!!.received(5)
+        verify(a, times(1))!!.received(6)
+        verify(a, times(0))!!.received(7)
+        verify(a, times(1))!!.error(any(javaClass<NullPointerException>()))
+    }
+
+    [Test]
+    public fun testScriptWithMaterialize() {
+        TestFactory().observable.materialize()!!.subscribe((received()))
+        verify(a, times(2))!!.received(any(javaClass<Notification<Int>>()))
+    }
+
+    [Test]
+    public fun testScriptWithMerge() {
+        val factory = TestFactory()
+        Observable.merge(factory.observable, factory.observable)!!.subscribe((received()))
+        verify(a, times(1))!!.received("hello_1")
+        verify(a, times(1))!!.received("hello_2")
+    }
+
+    [Test]
+    public fun testScriptWithMergeEx() {
+        val factory = TestFactory()
+        (factory.observable to factory.observable).merge().subscribe((received()))
+        verify(a, times(1))!!.received("hello_1")
+        verify(a, times(1))!!.received("hello_2")
+    }
+
+    [Test]
+    public fun testFromWithIterable() {
+        val list = listOf(1, 2, 3, 4, 5)
+        assertEquals(5, Observable.from(list)!!.count()!!.toBlockingObservable()!!.single())
+    }
+
+    [Test]
+    public fun testFromWithIterableEx() {
+        assertEquals(5, listOf(1, 2, 3, 4, 5).asObservable().count()!!.toBlockingObservable()!!.single())
+    }
+
+    [Test]
+    public fun testFromWithObjects() {
+        val list = listOf(1, 2, 3, 4, 5)
+        assertEquals(2, Observable.from(list, 6)!!.count()!!.toBlockingObservable()!!.single())
+    }
+
+    [Test]
+    public fun testStartWith() {
+        val list = listOf(10, 11, 12, 13, 14)
+        val startList = listOf(1, 2, 3, 4, 5)
+        assertEquals(6, Observable.from(list)!!.startWith(0)!!.count()!!.toBlockingObservable()!!.single())
+        assertEquals(10, Observable.from(list)!!.startWith(startList)!!.count()!!.toBlockingObservable()!!.single())
+    }
+
+    [Test]
+    public fun testStartWithEx() {
+        val list = listOf(10, 11, 12, 13, 14)
+        val startList = listOf(1, 2, 3, 4, 5)
+        assertEquals(6, list.asObservable().startWith(0)!!.count()!!.toBlockingObservable()!!.single())
+        assertEquals(10, list.asObservable().startWith(startList)!!.count()!!.toBlockingObservable()!!.single())
+    }
+
+    [Test]
+    public fun testScriptWithOnNext() {
+        TestFactory().observable.subscribe((received()))
+        verify(a, times(1))!!.received("hello_1")
+    }
+
+    [Test]
+    public fun testSkipTake() {
+        Observable.from(1, 2, 3)!!.skip(1)!!.take(1)!!.subscribe(received())
+        verify(a, times(0))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testSkipTakeEx() {
+        Triple(1, 2, 3).asObservable().skip(1)!!.take(1)!!.subscribe(received())
+        verify(a, times(0))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testSkip() {
+        Observable.from(1, 2, 3)!!.skip(2)!!.subscribe(received())
+        verify(a, times(0))!!.received(1)
+        verify(a, times(0))!!.received(2)
+        verify(a, times(1))!!.received(3)
+    }
+
+    [Test]
+    public fun testSkipEx() {
+        Triple(1, 2, 3).asObservable().skip(2)!!.subscribe(received())
+        verify(a, times(0))!!.received(1)
+        verify(a, times(0))!!.received(2)
+        verify(a, times(1))!!.received(3)
+    }
+
+    [Test]
+    public fun testTake() {
+        Observable.from(1, 2, 3)!!.take(2)!!.subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testTakeEx() {
+        Triple(1, 2, 3).asObservable().take(2)!!.subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testTakeLast() {
+        TestFactory().observable.takeLast(1)!!.subscribe((received()))
+        verify(a, times(1))!!.received("hello_1")
+    }
+
+    [Test]
+    public fun testTakeWhile() {
+        Observable.from(1, 2, 3)!!.takeWhile { x -> x!! < 3 }!!.subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testTakeWhileEx() {
+        Triple(1, 2, 3).asObservable().takeWhile { x -> x!! < 3 }!!.subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testTakeWhileWithIndex() {
+        Observable.from(1, 2, 3)!!.takeWhileWithIndex { x, i -> i!! < 2 }!!.subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testTakeWhileWithIndexEx() {
+        Triple(1, 2, 3).asObservable().takeWhileWithIndex { x, i -> i!! < 2 }!!.subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(0))!!.received(3)
+    }
+
+    [Test]
+    public fun testToSortedList() {
+        TestFactory().numbers.toSortedList()!!.subscribe(received())
+        verify(a, times(1))!!.received(listOf(1, 2, 3, 4, 5))
+    }
+
+    [Test]
+    public fun testForEach() {
+        asyncObservable.asObservable().toBlockingObservable()!!.forEach(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+        verify(a, times(1))!!.received(3)
+    }
+
+    [Test(expected = javaClass<RuntimeException>())]
+    public fun testForEachWithError() {
+        asyncObservable.asObservable().toBlockingObservable()!!.forEach { throw RuntimeException("err") }
+        fail("we expect an exception to be thrown")
+    }
+
+    [Test]
+    public fun testLastOrDefault() {
+        assertEquals("two", Observable.from("one", "two")!!.toBlockingObservable()!!.lastOrDefault("default") { x -> x!!.length == 3 })
+        assertEquals("default", Observable.from("one", "two")!!.toBlockingObservable()!!.lastOrDefault("default") { x -> x!!.length > 3 })
+    }
+
+    [Test]
+    public fun testLastOrDefaultEx() {
+        assertEquals("two", ("one" to"two").asObservable().toBlockingObservable()!!.lastOrDefault("default") { x -> x!!.length == 3 })
+        assertEquals("default", ("one" to"two").asObservable().toBlockingObservable()!!.lastOrDefault("default") { x -> x!!.length > 3 })
+    }
+
+    [Test(expected = javaClass<IllegalStateException>())]
+    public fun testSingle() {
+        assertEquals("one", Observable.from("one")!!.toBlockingObservable()!!.single { x -> x!!.length == 3 })
+        Observable.from("one", "two")!!.toBlockingObservable()!!.single { x -> x!!.length == 3 }
+        fail()
+    }
+
+    [Test]
+    public fun testDefer() {
+        Observable.defer { Observable.from(1, 2) }!!.subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+    }
+
+    [Test]
+    public fun testDeferEx() {
+        { (1 to 2).asObservable() }.defer().subscribe(received())
+        verify(a, times(1))!!.received(1)
+        verify(a, times(1))!!.received(2)
+    }
+
+    [Test]
+    public fun testAll() {
+        Observable.from(1, 2, 3)!!.all { x -> x!! > 0 }!!.subscribe(received())
+        verify(a, times(1))!!.received(true)
+    }
+
+    [Test]
+    public fun testAllEx() {
+        Triple(1, 2, 3).asObservable().all { x -> x!! > 0 }!!.subscribe(received())
+        verify(a, times(1))!!.received(true)
+    }
+
+    [Test]
+    public fun testZip() {
+        val o1 = Observable.from(1, 2, 3)!!
+        val o2 = Observable.from(4, 5, 6)!!
+        val o3 = Observable.from(7, 8, 9)!!
+
+        val values = Observable.zip(o1, o2, o3) { a, b, c -> listOf(a, b, c) }!!.toList()!!.toBlockingObservable()!!.single()!!
+        assertEquals(listOf(1, 4, 7), values[0])
+        assertEquals(listOf(2, 5, 8), values[1])
+        assertEquals(listOf(3, 6, 9), values[2])
+    }
+
+    [Test]
+    public fun testZipWithIterable() {
+        val o1 = Observable.from(1, 2, 3)!!
+        val o2 = Observable.from(4, 5, 6)!!
+        val o3 = Observable.from(7, 8, 9)!!
+
+        val values = Observable.zip(listOf(o1, o2, o3)) { args -> listOf(*args) }!!.toList()!!.toBlockingObservable()!!.single()!!
+        assertEquals(listOf(1, 4, 7), values[0])
+        assertEquals(listOf(2, 5, 8), values[1])
+        assertEquals(listOf(3, 6, 9), values[2])
+    }
+
+    [Test]
+    public fun testGroupBy() {
+        var count = 0
+
+        Observable.from("one", "two", "three", "four", "five", "six")!!
+                .groupBy { s -> s!!.length }!!
+                .mapMany { groupObervable ->
+            groupObervable!!.map { s ->
+                "Value: $s Group ${groupObervable.getKey()}"
+            }
+        }!!
+                .toBlockingObservable()!!.forEach { s ->
+            println(s)
+            count++
+        }
+
+        assertEquals(6, count)
+    }
+
     public trait ScriptAssertion{
-        fun error(e: Exception?)
+        fun error(e: Throwable?)
 
         fun received(e: Any?)
+    }
+
+    val funOnSubscribe: (Int, Observer<in String>) -> Subscription = { counter, observer ->
+        observer.onNext("hello_$counter")
+        observer.onCompleted()
+        Subscription { }
+    }
+
+    val asyncObservable: (Observer<in Int>) -> Subscription = { observer ->
+        thread {
+            Thread.sleep(50)
+            observer.onNext(1)
+            observer.onNext(2)
+            observer.onNext(3)
+            observer.onCompleted()
+        }
+        Subscriptions.empty()!!
+    }
+
+    /**
+     * Copied from (funKTionale)[https://github.com/MarioAriasC/funKTionale/blob/master/src/main/kotlin/org/funktionale/partials/namespace.kt]
+     */
+    public fun <P1, P2, R> Function2<P1, P2, R>.partially1(p1: P1): (P2) -> R {
+        return {(p2: P2) -> this(p1, p2) }
+    }
+
+    inner public class TestFactory(){
+        var counter = 1
+
+        val numbers: Observable<Int>
+            get(){
+                return listOf(1, 3, 2, 5, 4).asObservable()
+            }
+
+        val onSubscribe: (Observer<in String>) -> Subscription
+            get(){
+                return funOnSubscribe.partially1(counter++)
+            }
+
+        val observable: Observable<String>
+            get(){
+                return onSubscribe.asObservable()
+            }
+
     }
 }
