70c14a3424512c67db201f8ccca30d6a93307f8f
samuelgruetter <samuel.gruetter@epfl.ch>
2013-09-29 20:49:46 +0200

    TestScheduler example

--- language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/concurrency/TestScheduler.scala
+++ language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/concurrency/TestScheduler.scala
@@ -6,6 +6,40 @@ import org.scalatest.junit.JUnitSuite
 
 /**
  * Scheduler with artificial time, useful for testing.
+ * 
+ * For example, you could test the `Observable.interval` operation using a `TestScheduler` as follows:
+ * 
+ * {{{
+ * @Test def testInterval() {
+ *   import org.mockito.Matchers._
+ *   import org.mockito.Mockito._
+ *   
+ *   val scheduler = TestScheduler()
+ *   val observer = mock(classOf[rx.Observer[Long]])
+ *
+ *   val o = Observable.interval(1 second, scheduler)
+ *   val sub = o.subscribe(observer)
+ *
+ *   verify(observer, never).onNext(0L)
+ *   verify(observer, never).onCompleted()
+ *   verify(observer, never).onError(any(classOf[Throwable]))
+ *
+ *   scheduler.advanceTimeTo(2 seconds)
+ *
+ *   val inOrdr = inOrder(observer);
+ *   inOrdr.verify(observer, times(1)).onNext(0L)
+ *   inOrdr.verify(observer, times(1)).onNext(1L)
+ *   inOrdr.verify(observer, never).onNext(2L)
+ *   verify(observer, never).onCompleted()
+ *   verify(observer, never).onError(any(classOf[Throwable]))
+ *
+ *   sub.unsubscribe();
+ *   scheduler.advanceTimeTo(4 seconds)
+ *   verify(observer, never).onNext(2L)
+ *   verify(observer, times(1)).onCompleted()
+ *   verify(observer, never).onError(any(classOf[Throwable]))
+ * }
+ * }}}
  */
 class TestScheduler extends Scheduler {
   val asJava = new rx.concurrency.TestScheduler
@@ -33,44 +67,39 @@ object TestScheduler {
 }
 
 private class UnitTest extends JUnitSuite {
-  import org.mockito.Matchers._
-  import org.mockito.Mockito._
-  import org.junit.{Test, Before}
+  import org.junit.Test
   import scala.concurrent.duration._
   import scala.language.postfixOps
   import rx.lang.scala.{Observable, Observer}
 
-  var scheduler: TestScheduler = null
-  var observer: Observer[Long] = null
-  var observer2: Observer[Long] = null
-
-  @Before def before() {
-    scheduler = TestScheduler()
-    observer = mock(classOf[rx.Observer[Long]])
-  }
-
   @Test def testInterval() {
-    val w = Observable.interval(1 second, scheduler)
-    val sub = w.subscribe(observer)
+    import org.mockito.Matchers._
+    import org.mockito.Mockito._
+    
+    val scheduler = TestScheduler()
+    val observer = mock(classOf[rx.Observer[Long]])
 
-    verify(observer, never()).onNext(0L)
-    verify(observer, never()).onCompleted()
-    verify(observer, never()).onError(any(classOf[Throwable]))
+    val o = Observable.interval(1 second, scheduler)
+    val sub = o.subscribe(observer)
+
+    verify(observer, never).onNext(0L)
+    verify(observer, never).onCompleted()
+    verify(observer, never).onError(any(classOf[Throwable]))
 
     scheduler.advanceTimeTo(2 seconds)
 
     val inOrdr = inOrder(observer);
     inOrdr.verify(observer, times(1)).onNext(0L)
     inOrdr.verify(observer, times(1)).onNext(1L)
-    inOrdr.verify(observer, never()).onNext(2L)
-    verify(observer, never()).onCompleted()
-    verify(observer, never()).onError(any(classOf[Throwable]))
+    inOrdr.verify(observer, never).onNext(2L)
+    verify(observer, never).onCompleted()
+    verify(observer, never).onError(any(classOf[Throwable]))
 
     sub.unsubscribe();
     scheduler.advanceTimeTo(4 seconds)
-    verify(observer, never()).onNext(2L)
+    verify(observer, never).onNext(2L)
     verify(observer, times(1)).onCompleted()
-    verify(observer, never()).onError(any(classOf[Throwable]))
+    verify(observer, never).onError(any(classOf[Throwable]))
   }
 }
 
