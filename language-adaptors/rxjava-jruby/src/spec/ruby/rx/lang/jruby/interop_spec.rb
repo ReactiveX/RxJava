16a773544793491d525a190c1b3baa1c10fec613
Mike Ragalie <michael.ragalie@verbasoftware.com>
2013-10-06 17:49:37 -0700

    JRuby license headers

--- language-adaptors/rxjava-jruby/src/spec/ruby/rx/lang/jruby/interop_spec.rb
+++ language-adaptors/rxjava-jruby/src/spec/ruby/rx/lang/jruby/interop_spec.rb
@@ -1,3 +1,17 @@
+# Copyright 2013 Mike Ragalie
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require_relative "spec_helper"
 
 describe Rx::Lang::Jruby::Interop do
@@ -31,6 +45,7 @@ describe Rx::Lang::Jruby::Interop do
 
     it "doesn't wrap OnSubscribeFunc arguments" do
       proc = lambda {|observer| observer.onNext("hi")}
+      Java::Rx::Observable.__persistent__ = true
       Java::Rx::Observable.should_not_receive(:create_without_wrapping)
       Java::Rx::Observable.create(proc).should be_a(Java::Rx::Observable)
     end
