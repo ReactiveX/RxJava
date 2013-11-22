5796e71d5c259bbbb8a0fd1cf09013a18211e33c
samuelgruetter <samuel.gruetter@epfl.ch>
2013-11-22 20:00:51 +0100

    update package objects

--- language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/subjects/package.scala
+++ language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/subjects/package.scala
@@ -1,31 +1,6 @@
 package rx.lang.scala
 
 /**
- * Provides the type `Subject`.
+ * Subjects are Observers and Observables at the same time.
  */
-package object subjects {
-
-  /**
-   * A Subject is an Observable and an Observer at the same time. 
-   * 
-   * The Java Subject looks like this: 
-   * {{{
-   * public abstract class Subject<T,R> extends Observable<R> implements Observer<T>
-   * }}}
-   */
-  type Subject[-T, +R] = rx.subjects.Subject[_ >: T, _ <: R]
-
-  // For nicer scaladoc, we would like to present something like this:
-  /*
-  trait Observable[+R] {}
-  trait Observer[-T] {}
-  trait Subject[-T, +R] extends Observable[R] with Observer[T] { }
-  */
-  
-  // We don't make aliases to these types, because they are considered internal/not needed by users:
-  // rx.subjects.AsyncSubject
-  // rx.subjects.BehaviorSubject
-  // rx.subjects.PublishSubject
-  // rx.subjects.ReplaySubject
-
-}
+package object subjects {}
