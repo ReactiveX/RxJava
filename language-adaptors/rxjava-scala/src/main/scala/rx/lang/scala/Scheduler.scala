ca2ce540d3c44d4155fc979b8216e2c5c169fa00
headinthebox <emeijer@applied-duality.com>
2013-11-29 00:25:45 -0800

    Fixed doc comment.

--- language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/Scheduler.scala
+++ language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/Scheduler.scala
@@ -20,8 +20,7 @@ import scala.concurrent.duration.Duration
 import rx.util.functions.{Action0, Action1, Func2}
 
 /**
- * Represents an object   rx.lang.scala.ImplicitFunctionConversions
- schedules units of work.
+ * Represents an object that schedules units of work.
  */
 trait Scheduler {
   def asJavaScheduler: rx.Scheduler
