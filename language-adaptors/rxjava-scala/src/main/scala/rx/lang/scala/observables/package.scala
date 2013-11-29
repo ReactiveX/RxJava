fc8ea03005b01afd67cea80546702459f8325bc5
headinthebox <emeijer@applied-duality.com>
2013-11-29 12:36:53 -0800

    Added copyright headers

--- language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/observables/package.scala
+++ language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/observables/package.scala
@@ -18,7 +18,7 @@ package rx.lang.scala
 /**
  * Contains special Observables.
  * 
- * In Scala, this package only contains [[BlockingObservable]].
+ * In Scala, this package only contains [[rx.lang.scala.observables.BlockingObservable]].
  * In the corresponding Java package `rx.observables`, there is also a
  * `GroupedObservable` and a `ConnectableObservable`, but these are not needed
  * in Scala, because we use a pair `(key, observable)` instead of `GroupedObservable`
