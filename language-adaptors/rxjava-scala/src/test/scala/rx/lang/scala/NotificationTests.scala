168279e0956489ff67416500ca8050d6a341ad04
headinthebox <emeijer@applied-duality.com>
2013-12-07 12:31:49 -0800

    Notification tests.

--- language-adaptors/rxjava-scala/src/test/scala/rx/lang/scala/NotificationTests.scala
+++ language-adaptors/rxjava-scala/src/test/scala/rx/lang/scala/NotificationTests.scala
@@ -8,7 +8,7 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 import org.mockito.Mockito._
 import org.mockito.Matchers._
-import rx.lang.scala.Notification.{OnError, OnNext}
+import rx.lang.scala.Notification.{OnCompleted, OnError, OnNext}
 
 
 class NotificationTests extends JUnitSuite {
@@ -17,5 +17,27 @@ class NotificationTests extends JUnitSuite {
 
     val onNext = OnNext(42)
     Assert.assertEquals(42, onNext match { case OnNext(value) => value })
+
+    val oops = new Exception("Oops")
+    val onError = OnError(oops)
+    Assert.assertEquals(oops, onError match { case OnError(error) => error })
+
+    val onCompleted = OnCompleted()
+    Assert.assertEquals((), onCompleted match { case OnCompleted() => () })
+  }
+
+  @Test
+  def accept() {
+
+    val onNext = OnNext(42)
+    Assert.assertEquals(42, onNext.accept(x=>42, e=>4711,()=>13))
+
+    val oops = new Exception("Oops")
+    val onError = OnError(oops)
+    Assert.assertEquals(4711, onError.accept(x=>42, e=>4711,()=>13))
+
+    val onCompleted = OnCompleted()
+    Assert.assertEquals(13, onCompleted.accept(x=>42, e=>4711,()=>13))
+
   }
 }
