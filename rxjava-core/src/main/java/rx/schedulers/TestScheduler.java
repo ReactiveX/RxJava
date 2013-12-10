d42b6453a35e6aba72d467e88997a0adcb57b916
Ben Christensen <benjchristensen@netflix.com>
2013-12-10 15:12:59 -0800

    Migrate Schedulers to rx.schedulers package

--- rxjava-core/src/main/java/rx/schedulers/TestScheduler.java
+++ rxjava-core/src/main/java/rx/schedulers/TestScheduler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package rx.concurrency;
+package rx.schedulers;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
