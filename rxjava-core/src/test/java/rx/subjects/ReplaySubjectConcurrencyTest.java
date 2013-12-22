dba5de963a8a04445feb09d533998aef4a7227f2
Ben Christensen <benjchristensen@netflix.com>
2013-12-21 21:01:39 -0800

    License Header and Separate out Perf Tests

--- rxjava-core/src/test/java/rx/subjects/ReplaySubjectConcurrencyTest.java
+++ rxjava-core/src/test/java/rx/subjects/ReplaySubjectConcurrencyTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rx.subjects;
 
 import static org.junit.Assert.*;
@@ -18,14 +33,14 @@ import rx.Subscription;
 import rx.subscriptions.Subscriptions;
 import rx.util.functions.Action1;
 
-public class ReplaySubjectPerformanceAndConcurrencyTest {
+public class ReplaySubjectConcurrencyTest {
 
     public static void main(String args[]) {
         try {
             for (int i = 0; i < 100; i++) {
-                new ReplaySubjectPerformanceAndConcurrencyTest().testSubscribeCompletionRaceCondition();
-                new ReplaySubjectPerformanceAndConcurrencyTest().testReplaySubjectConcurrentSubscriptions();
-                new ReplaySubjectPerformanceAndConcurrencyTest().testReplaySubjectConcurrentSubscribersDoingReplayDontBlockEachOther();
+                new ReplaySubjectConcurrencyTest().testSubscribeCompletionRaceCondition();
+                new ReplaySubjectConcurrencyTest().testReplaySubjectConcurrentSubscriptions();
+                new ReplaySubjectConcurrencyTest().testReplaySubjectConcurrentSubscribersDoingReplayDontBlockEachOther();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
