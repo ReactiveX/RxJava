ac83ed9b44d2e1ad7f066bd794b4c676c5a86adf
Ben Christensen <benjchristensen@netflix.com>
2014-02-06 23:06:01 -0800

    Make Parallel use NewThread until Computation Fixed
    See https://github.com/Netflix/RxJava/issues/713
    It was causing non-deterministic behavior, random test failures and poor performance.

--- rxjava-core/src/main/java/rx/Observable.java
+++ rxjava-core/src/main/java/rx/Observable.java
@@ -5296,7 +5296,9 @@ public class Observable<T> {
      * @see <a href="https://github.com/Netflix/RxJava/wiki/Observable-Utility-Operators#wiki-parallel">RxJava Wiki: parallel()</a>
      */
     public final <R> Observable<R> parallel(Func1<Observable<T>, Observable<R>> f) {
-        return lift(new OperatorParallel<T, R>(f, Schedulers.computation()));
+        // TODO move this back to Schedulers.computation() again once that is properly using eventloops
+        // see https://github.com/Netflix/RxJava/issues/713 for why this was changed
+        return lift(new OperatorParallel<T, R>(f, Schedulers.newThread()));
     }
 
     /**
