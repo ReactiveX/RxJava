7c16c51c345dcf41f7f0c232c048e2b0d9c528a4
Ben Christensen <benjchristensen@netflix.com>
2014-03-12 09:50:11 -0700

    Unit tests for each

--- rxjava-core/src/test/java/rx/observers/SerializedObserverViaStateMachineTest.java
+++ rxjava-core/src/test/java/rx/observers/SerializedObserverViaStateMachineTest.java
@@ -1,5 +1,10 @@
 package rx.observers;
 
-public class SerializedObserverViaStateMachineTest extends SerializedObserverViaQueueAndCounterTest {
+import rx.Observer;
 
+public class SerializedObserverViaStateMachineTest extends SerializedObserverTest {
+    @Override
+    protected Observer<String> serializedObserver(Observer<String> o) {
+        return new SerializedObserverViaStateMachine<String>(o);
+    }
 }
