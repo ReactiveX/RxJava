a07e83701060fc02746e9f0a6c630fccde49ed0f
David Gross <davgross@netflix.com>
2014-07-24 10:06:59 -0700

    removing unicode ligature that breaks build

--- rxjava-core/src/main/java/rx/internal/util/unsafe/SpscArrayQueue.java
+++ rxjava-core/src/main/java/rx/internal/util/unsafe/SpscArrayQueue.java
@@ -100,7 +100,7 @@ abstract class SpscArrayQueueL3Pad<E> extends SpscArrayQueueConsumerField<E> {
  * variation on Fast Flow).<br>
  * For convenience the relevant papers are available in the resources folder:</br>
  * <i>2010 - Pisa - SPSC Queues on Shared Cache Multi-Core Systems.pdf</br>
- * 2012 - Junchang- BQueue- Efﬁcient and Practical Queuing.pdf </br></i>
+ * 2012 - Junchang- BQueue- Efficient and Practical Queuing.pdf </br></i>
  * This implementation is wait free.
  * 
  * @author nitsanw
