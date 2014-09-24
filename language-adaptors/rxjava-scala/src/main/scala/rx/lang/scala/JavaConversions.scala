df18366ed48bc7b58efb36d09e32893456fb4391
zsxwing <zsxwing@gmail.com>
2014-09-24 23:44:22 +0800

    Fix the compose generics in RxScala

--- language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/JavaConversions.scala
+++ language-adaptors/rxjava-scala/src/main/scala/rx/lang/scala/JavaConversions.scala
@@ -65,8 +65,8 @@ object JavaConversions {
 
   implicit def toJavaTransformer[T, R](transformer: Observable[T] => Observable[R]): rx.Observable.Transformer[T, R] = {
     new rx.Observable.Transformer[T, R] {
-      override def call(o: rx.Observable[_ <: T]): rx.Observable[R] = {
-        transformer(toScalaObservable(o)).asJavaObservable.asInstanceOf[rx.Observable[R]]
+      override def call(o: rx.Observable[_ <: T]): rx.Observable[_ <: R] = {
+        transformer(toScalaObservable(o)).asJavaObservable
       }
     }
   }
