14fcc22ec431e87b88f99a7d5498c321127dd48d
Jason Neufeld <jneufeld@google.com>
2015-01-20 13:13:55 -0800

    Use unsubscribed check instead of a null check.

--- src/main/java/rx/subscriptions/CompositeSubscription.java
+++ src/main/java/rx/subscriptions/CompositeSubscription.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014 Netflix, Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -55,8 +55,8 @@ public final class CompositeSubscription implements Subscription {
      *          the {@link Subscription} to add
      */
     public void add(final Subscription s) {
-        if (s == null) {
-            throw new NullPointerException("Added Subscription cannot be null.");
+        if (s.isUnsubscribed()) {
+            return;
         }
         Subscription unsubscribe = null;
         synchronized (this) {
