84965829df9d24a65c4e4afce473fdec3efdf4c3
Dave Moten <davidmoten@gmail.com>
2015-01-29 17:35:00 +1100

    add request overflow check for OnSubscribeFromIterable

--- src/main/java/rx/Producer.java
+++ src/main/java/rx/Producer.java
@@ -23,6 +23,16 @@ public interface Producer {
     /**
      * Request a certain maximum number of items from this Producer. This is a way of requesting backpressure.
      * To disable backpressure, pass {@code Long.MAX_VALUE} to this method.
+     * <p>
+     * Requests are additive but if a sequence of requests totals more than {@code Long.MAX_VALUE} then 
+     * {@code Long.MAX_VALUE} requests will be actioned and the extras <i>may</i> be ignored. Arriving at 
+     * {@code Long.MAX_VALUE} by addition of requests cannot be assumed to disable backpressure. For example, 
+     * the code below may result in {@code Long.MAX_VALUE} requests being actioned only.
+     * 
+     * <pre>
+     * request(100);
+     * request(Long.MAX_VALUE-1);
+     * </pre> 
      *
      * @param n the maximum number of items you want this Producer to produce, or {@code Long.MAX_VALUE} if you
      *          want the Producer to produce items at its own pace
