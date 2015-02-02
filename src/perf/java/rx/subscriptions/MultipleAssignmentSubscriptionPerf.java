152167e803a616ff5a330f83a80bb142a42373bf
akarnokd <akarnokd@akarnokd-PC2>
2015-02-02 23:40:03 +0100

    Added perf tests for various container-like subscriptions

---
+++ src/perf/java/rx/subscriptions/MultipleAssignmentSubscriptionPerf.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2014 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rx.subscriptions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import rx.Subscription;
+
+/**
+ * Benchmark typical multiple-assignment subscription behavior.
+ * <p>
+ * gradlew benchmarks "-Pjmh=-f 1 -tu s -bm thrpt -wi 5 -i 5 -r 1 .*MultipleAssignmentSubscriptionPerf.*"
+ * <p>
+ * gradlew benchmarks "-Pjmh=-f 1 -tu ns -bm avgt -wi 5 -i 5 -r 1 .*MultipleAssignmentSubscriptionPerf.*"
+ */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class MultipleAssignmentSubscriptionPerf {
+    @State(Scope.Thread)
+    public static class TheState {
+        @Param({ "1", "1000", "100000" })
+        public int loop;
+        @Param({ "1", "5", "10", "100" })
+        public int count;
+        
+        public final MultipleAssignmentSubscription csub = new MultipleAssignmentSubscription();
+        
+        public Subscription[] values;
+        @Setup
+        public void setup() {
+            values = new Subscription[count];
+            for (int i = 0; i < count; i++) {
+                values[i] = new Subscription() {
+                    @Override
+                    public boolean isUnsubscribed() {
+                        return false;
+                    }
+                    @Override
+                    public void unsubscribe() {
+                        
+                    }
+                };
+            }
+        }
+    }
+    @Benchmark
+    public void add(TheState state) {
+        MultipleAssignmentSubscription csub = state.csub;
+        Subscription[] values = state.values;
+        
+        for (int i = state.loop; i > 0; i--) {
+            for (int j = values.length - 1; j >= 0; j--) {
+                csub.set(state.values[j]);
+            }
+        }
+    }
+    @Benchmark
+    public void addLocal(TheState state) {
+        MultipleAssignmentSubscription csub = new MultipleAssignmentSubscription();
+        Subscription[] values = state.values;
+        
+        for (int i = state.loop; i > 0; i--) {
+            for (int j = values.length - 1; j >= 0; j--) {
+                csub.set(state.values[j]);
+            }
+        }
+    }
+}
---
+++ src/perf/java/rx/subscriptions/SubscriptionListConcurrentPerf.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright 2014 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rx.subscriptions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Group;
+import org.openjdk.jmh.annotations.GroupThreads;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+
+import rx.Subscription;
+import rx.internal.util.SubscriptionList;
+
+/**
+ * Benchmark typical subscription list concurrent behavior.
+ * <p>
+ * gradlew benchmarks "-Pjmh=-f 1 -tu s -bm thrpt -wi 5 -i 5 -r 1 .*CompositeSubscriptionConcurrentPerf.*"
+ * <p>
+ * gradlew benchmarks "-Pjmh=-f 1 -tu ns -bm avgt -wi 5 -i 5 -r 1 .*CompositeSubscriptionConcurrentPerf.*"
+ */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Threads(2)
+@State(Scope.Group)
+public class SubscriptionListConcurrentPerf {
+    @Param({ "1", "1000", "100000" })
+    public int loop;
+    
+    public final SubscriptionList csub = new SubscriptionList();
+    @Param({ "1", "5", "10", "20" })
+    public int count;
+    
+    public Subscription[] values;
+    @Setup
+    public void setup() {
+        values = new Subscription[count * 2];
+        for (int i = 0; i < count * 2; i++) {
+            values[i] = new Subscription() {
+                @Override
+                public boolean isUnsubscribed() {
+                    return false;
+                }
+                @Override
+                public void unsubscribe() {
+                    
+                }
+            };
+        }
+    }
+
+    @Group("g1")
+    @GroupThreads(1)
+    @Benchmark
+    public void addClearT1() {
+        SubscriptionList csub = this.csub;
+        Subscription[] values = this.values;
+        
+        for (int i = loop; i > 0; i--) {
+            for (int j = values.length - 1; j >= 0; j--) {
+                csub.add(values[j]);
+            }
+            csub.clear();
+        }
+    }
+    @Group("g1")
+    @GroupThreads(1)
+    @Benchmark
+    public void addClearT2() {
+        SubscriptionList csub = this.csub;
+        Subscription[] values = this.values;
+        
+        for (int i = loop; i > 0; i--) {
+            for (int j = values.length - 1; j >= 0; j--) {
+                csub.add(values[j]);
+            }
+            csub.clear();
+        }
+    }
+    @Group("g2")
+    @GroupThreads(1)
+    @Benchmark
+    public void addClearHalfT1() {
+        SubscriptionList csub = this.csub;
+        Subscription[] values = this.values;
+        int n = values.length;
+        
+        for (int i = loop; i > 0; i--) {
+            for (int j = n / 2 - 1; j >= 0; j--) {
+                csub.add(values[j]);
+            }
+            csub.clear();
+        }
+    }
+    @Group("g2")
+    @GroupThreads(1)
+    @Benchmark
+    public void addRemoveHalfT2() {
+        SubscriptionList csub = this.csub;
+        Subscription[] values = this.values;
+        int n = values.length;
+        
+        for (int i = loop; i > 0; i--) {
+            for (int j = n - 1; j >= n / 2; j--) {
+                csub.add(values[j]);
+            }
+            csub.clear();
+        }
+    }
+}
