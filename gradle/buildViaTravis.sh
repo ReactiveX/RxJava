a8c306b0eedf04396a96264c8473d0eb9fd3b86c
Ben Christensen <benjchristensen@gmail.com>
2015-02-04 10:56:14 -0800

    Add Snapshot Support

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script will build the project.
 
-echo -e 'Build Script => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-
-./gradlew -Prelease.useLastTag=true build
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  ./gradlew -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" snapshot --stacktrace
+else
+  echo -e 'Build Pull Request => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  ./gradlew -Prelease.useLastTag=true build
+fi
