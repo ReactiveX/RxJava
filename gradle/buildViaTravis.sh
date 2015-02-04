b3640d8949b4660aa6fa83c988447f250e0a6921
Ben Christensen <benjchristensen@gmail.com>
2015-02-04 11:26:43 -0800

    Add Release Branch

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -2,12 +2,14 @@
 # This script will build the project.
 
 if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
-  echo -e 'Build Pull Request => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  echo -e 'Build Pull Request => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.useLastTag=true build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" snapshot --stacktrace
-else
+elif if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -Prelease.useLastTag=true build
+  ./gradlew -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" final --stacktrace
+else
+  echo -e 'ERROR: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
 fi
