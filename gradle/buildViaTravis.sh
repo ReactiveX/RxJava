bb5fd6b453e4632d36ebc8f07b9c7dfe5eba7965
Ben Christensen <benjchristensen@gmail.com>
2015-02-11 14:03:14 -0800

    Fix Pull Request Conditionals

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # This script will build the project.
 
-if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
-  echo -e 'Build Pull Request => Branch ['$TRAVIS_BRANCH']'
+if [ "$TRAVIS_PULL_REQUEST" ]; then
+  echo -e 'Build Pull Request ['$TRAVIS_PULL_REQUEST'] => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.useLastTag=true build
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+elif [ ! "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" snapshot --stacktrace
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+elif [ ! "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" final --stacktrace
 else
-  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
   ./gradlew -Prelease.useLastTag=true build
 fi
