a5d55dc4efebc29e387dc8c828c56485dfabf4bf
akarnokd <akarnokd@192.168.0.6>
2015-09-07 22:14:27 +0200

    2.x: operator test: serialize, single + SerializedSubscriber fix

---
+++ src/test/java/io/reactivex/internal/operators/OperatorSingleTest.java
@@ -0,0 +1,416 @@
+package io.reactivex.internal.operators;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.*;
+
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.reactivestreams.Subscriber;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.TestHelper;
+
+public class OperatorSingleTest {
+
+    @Test
+    public void testSingle() {
+        Observable<Integer> observable = Observable.just(1).single();
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onNext(1);
+        inOrder.verify(observer, times(1)).onComplete();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleWithTooManyElements() {
+        Observable<Integer> observable = Observable.just(1, 2).single();
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onError(
+                isA(IllegalArgumentException.class));
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleWithEmpty() {
+        Observable<Integer> observable = Observable.<Integer> empty().single();
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onError(
+                isA(NoSuchElementException.class));
+        inOrder.verifyNoMoreInteractions();
+    }
+    
+    @Test
+    public void testSingleDoesNotRequestMoreThanItNeedsToEmitItem() {
+        final AtomicLong request = new AtomicLong();
+        Observable.just(1).doOnRequest(new LongConsumer() {
+            @Override
+            public void accept(long n) {
+                request.addAndGet(n);
+            }
+        }).toBlocking().single();
+        // FIXME single now triggers fast-path
+        assertEquals(Long.MAX_VALUE, request.get());
+    }
+
+    @Test
+    public void testSingleDoesNotRequestMoreThanItNeedsToEmitErrorFromEmpty() {
+        final AtomicLong request = new AtomicLong();
+        try {
+            Observable.empty().doOnRequest(new LongConsumer() {
+                @Override
+                public void accept(long n) {
+                    request.addAndGet(n);
+                }
+            }).toBlocking().single();
+        } catch (NoSuchElementException e) {
+            // FIXME single now triggers fast-path
+            assertEquals(Long.MAX_VALUE, request.get());
+        }
+    }
+
+    @Test
+    public void testSingleDoesNotRequestMoreThanItNeedsToEmitErrorFromMoreThanOne() {
+        final AtomicLong request = new AtomicLong();
+        try {
+            Observable.just(1, 2).doOnRequest(new LongConsumer() {
+                @Override
+                public void accept(long n) {
+                    request.addAndGet(n);
+                }
+            }).toBlocking().single();
+        } catch (IllegalArgumentException e) {
+            // FIXME single now triggers fast-path
+            assertEquals(Long.MAX_VALUE, request.get());
+        }
+    }
+    
+    @Test
+    public void testSingleDoesNotRequestMoreThanItNeedsIf1Then2Requested() {
+        final List<Long> requests = new ArrayList<>();
+        Observable.just(1)
+        //
+                .doOnRequest(new LongConsumer() {
+                    @Override
+                    public void accept(long n) {
+                        requests.add(n);
+                    }
+                })
+                //
+                .single()
+                //
+                .subscribe(new Observer<Integer>() {
+
+                    @Override
+                    public void onStart() {
+                        request(1);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer t) {
+                        request(2);
+                    }
+                });
+        // FIXME single now triggers fast-path
+        assertEquals(Arrays.asList(Long.MAX_VALUE, Long.MAX_VALUE), requests);
+    }
+    
+    @Test
+    public void testSingleDoesNotRequestMoreThanItNeedsIf3Requested() {
+        final List<Long> requests = new ArrayList<>();
+        Observable.just(1)
+        //
+                .doOnRequest(new LongConsumer() {
+                    @Override
+                    public void accept(long n) {
+                        requests.add(n);
+                    }
+                })
+                //
+                .single()
+                //
+                .subscribe(new Observer<Integer>() {
+
+                    @Override
+                    public void onStart() {
+                        request(3);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer t) {
+                    }
+                });
+        // FIXME single now triggers fast-path
+        assertEquals(Arrays.asList(Long.MAX_VALUE), requests);
+    }
+    
+    @Test
+    public void testSingleRequestsExactlyWhatItNeedsIf1Requested() {
+        final List<Long> requests = new ArrayList<>();
+        Observable.just(1)
+        //
+                .doOnRequest(new LongConsumer() {
+                    @Override
+                    public void accept(long n) {
+                        requests.add(n);
+                    }
+                })
+                //
+                .single()
+                //
+                .subscribe(new Observer<Integer>() {
+
+                    @Override
+                    public void onStart() {
+                        request(1);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer t) {
+                    }
+                });
+        // FIXME single now triggers fast-path
+        assertEquals(Arrays.asList(Long.MAX_VALUE), requests);
+    }
+
+
+    @Test
+    public void testSingleWithPredicate() {
+        Observable<Integer> observable = Observable.just(1, 2)
+                .filter(
+                new Predicate<Integer>() {
+
+                    @Override
+                    public boolean test(Integer t1) {
+                        return t1 % 2 == 0;
+                    }
+                })
+                .single();
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onNext(2);
+        inOrder.verify(observer, times(1)).onComplete();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleWithPredicateAndTooManyElements() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4)
+                .filter(
+                new Predicate<Integer>() {
+
+                    @Override
+                    public boolean test(Integer t1) {
+                        return t1 % 2 == 0;
+                    }
+                })
+                .single();
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onError(
+                isA(IllegalArgumentException.class));
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleWithPredicateAndEmpty() {
+        Observable<Integer> observable = Observable.just(1)
+                .filter(
+                new Predicate<Integer>() {
+
+                    @Override
+                    public boolean test(Integer t1) {
+                        return t1 % 2 == 0;
+                    }
+                })
+                .single();
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onError(
+                isA(NoSuchElementException.class));
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleOrDefault() {
+        Observable<Integer> observable = Observable.just(1).single(2);
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onNext(1);
+        inOrder.verify(observer, times(1)).onComplete();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleOrDefaultWithTooManyElements() {
+        Observable<Integer> observable = Observable.just(1, 2).single(3);
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onError(
+                isA(IllegalArgumentException.class));
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleOrDefaultWithEmpty() {
+        Observable<Integer> observable = Observable.<Integer> empty()
+                .single(1);
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onNext(1);
+        inOrder.verify(observer, times(1)).onComplete();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleOrDefaultWithPredicate() {
+        Observable<Integer> observable = Observable.just(1, 2)
+                .filter(t1 -> t1 % 2 == 0)
+                .single(4);
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onNext(2);
+        inOrder.verify(observer, times(1)).onComplete();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleOrDefaultWithPredicateAndTooManyElements() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4)
+                .filter(t1 -> t1 % 2 == 0)
+                .single(6);
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onError(
+                isA(IllegalArgumentException.class));
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleOrDefaultWithPredicateAndEmpty() {
+        Observable<Integer> observable = Observable.just(1)
+                .filter(t1 -> t1 % 2 == 0)
+                .single(2);
+
+        Subscriber<Integer> observer = TestHelper.mockSubscriber();
+        observable.subscribe(observer);
+
+        InOrder inOrder = inOrder(observer);
+        inOrder.verify(observer, times(1)).onNext(2);
+        inOrder.verify(observer, times(1)).onComplete();
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSingleWithBackpressure() {
+        Observable<Integer> observable = Observable.just(1, 2).single();
+
+        Subscriber<Integer> subscriber = spy(new Observer<Integer>() {
+
+            @Override
+            public void onStart() {
+                request(1);
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                request(1);
+            }
+        });
+        observable.subscribe(subscriber);
+
+        InOrder inOrder = inOrder(subscriber);
+        inOrder.verify(subscriber, times(1)).onError(isA(IllegalArgumentException.class));
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test(timeout = 30000)
+    public void testIssue1527() throws InterruptedException {
+        //https://github.com/ReactiveX/RxJava/pull/1527
+        Observable<Integer> source = Observable.just(1, 2, 3, 4, 5, 6);
+        Observable<Integer> reduced = source.reduce((i1, i2) -> i1 + i2);
+
+        Integer r = reduced.toBlocking().first();
+        assertEquals(21, r.intValue());
+    }
+}
