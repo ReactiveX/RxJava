3d53506eb449e7b7ac338bfaecfd69f292d2b87d
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2015-09-20 04:10:28 +0300

    Possible multithreading bug fix in OnSubscribeAmb

--- src/main/java/rx/internal/operators/OnSubscribeAmb.java
+++ src/main/java/rx/internal/operators/OnSubscribeAmb.java
@@ -269,7 +269,7 @@ public final class OnSubscribeAmb<T> implements OnSubscribe<T>{
 
         private final Subscriber<? super T> subscriber;
         private final Selection<T> selection;
-        private boolean chosen;
+        private volatile boolean chosen;
 
         private AmbSubscriber(long requested, Subscriber<? super T> subscriber, Selection<T> selection) {
             this.subscriber = subscriber;
