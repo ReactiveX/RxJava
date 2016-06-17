bef94c7e0506895be9cc5a0140c30bfaaa3e1108
David Andrews <dta@alumni.cmu.edu>
2016-06-17 03:12:19 -0400

    Update AsyncSubject.java

--- src/main/java/rx/subjects/AsyncSubject.java
+++ src/main/java/rx/subjects/AsyncSubject.java
@@ -28,7 +28,8 @@ import rx.subjects.SubjectSubscriptionManager.SubjectObserver;
 /**
  * Subject that publishes only the last item observed to each {@link Observer} once the source {@code Observable}
  * has completed.  The item is cached and published to any {@code Observer}s which subscribe after the source
- * has completed.  If the source emitted no items, {@code AsyncSubject} completes without emitting anything.  
+ * has completed.  If the source emitted no items, {@code AsyncSubject} completes without emitting anything.
+ * If the source terminated in an error, current and future subscribers will receive only the error.
  * <p>
  * <img width="640" src="https://raw.github.com/wiki/ReactiveX/RxJava/images/rx-operators/S.AsyncSubject.png" alt="">
  * <p>
