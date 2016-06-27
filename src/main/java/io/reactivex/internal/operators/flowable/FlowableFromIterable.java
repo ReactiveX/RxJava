13df09b14d97820d5becb6d9ceacfeff5091a8e3
David Karnok <akarnokd@gmail.com>
2016-06-27 14:54:37 +0200

    2.x: enable fuseable sources, adjust operators (#4136)

--- src/main/java/io/reactivex/internal/operators/flowable/FlowableFromIterable.java
+++ src/main/java/io/reactivex/internal/operators/flowable/FlowableFromIterable.java
@@ -14,13 +14,14 @@
 package io.reactivex.internal.operators.flowable;
 
 import java.util.Iterator;
-import java.util.concurrent.atomic.AtomicLong;
 
-import org.reactivestreams.*;
+import org.reactivestreams.Subscriber;
 
 import io.reactivex.Flowable;
+import io.reactivex.internal.functions.Objects;
+import io.reactivex.internal.fuseable.ConditionalSubscriber;
 import io.reactivex.internal.subscriptions.*;
-import io.reactivex.internal.util.BackpressureHelper;
+import io.reactivex.internal.util.*;
 
 public final class FlowableFromIterable<T> extends Flowable<T> {
     
@@ -32,102 +33,323 @@ public final class FlowableFromIterable<T> extends Flowable<T> {
     @Override
     public void subscribeActual(Subscriber<? super T> s) {
         Iterator<? extends T> it;
-        try {
-            it = source.iterator();
-        } catch (Throwable e) {
-            EmptySubscription.error(e, s);
-            return;
-        }
         boolean hasNext;
         try {
+            it = source.iterator();
+
             hasNext = it.hasNext();
         } catch (Throwable e) {
+            Exceptions.throwIfFatal(e);
             EmptySubscription.error(e, s);
             return;
         }
+        
         if (!hasNext) {
             EmptySubscription.complete(s);
             return;
         }
-        s.onSubscribe(new IteratorSourceSubscription<T>(it, s));
+        
+        if (s instanceof ConditionalSubscriber) {
+            s.onSubscribe(new IteratorConditionalSubscription<T>(
+                    (ConditionalSubscriber<? super T>)s, it));
+        } else {
+            s.onSubscribe(new IteratorSubscription<T>(s, it));
+        }
+        
     }
     
-    static final class IteratorSourceSubscription<T> extends AtomicLong implements Subscription {
+    static abstract class BaseRangeSubscription<T> extends BasicQueueSubscription<T> {
         /** */
-        private static final long serialVersionUID = 8931425802102883003L;
+        private static final long serialVersionUID = -2252972430506210021L;
+
         final Iterator<? extends T> it;
-        final Subscriber<? super T> subscriber;
         
         volatile boolean cancelled;
         
-        public IteratorSourceSubscription(Iterator<? extends T> it, Subscriber<? super T> subscriber) {
+        boolean once;
+        
+        public BaseRangeSubscription(Iterator<? extends T> it) {
             this.it = it;
-            this.subscriber = subscriber;
         }
+        
         @Override
-        public void request(long n) {
-            if (!SubscriptionHelper.validateRequest(n)) {
-                return;
+        public final int requestFusion(int mode) {
+            return mode & SYNC;
+        }
+
+        @Override
+        public final T poll() {
+            if (!once) {
+                once = true;
+            } else {
+                if (!it.hasNext()) {
+                    return null;
+                }
             }
-            if (BackpressureHelper.add(this, n) != 0L) {
-                return;
+            return Objects.requireNonNull(it.next(), "Iterator.next() returned a null value");
+        }
+
+        
+        @Override
+        public final boolean isEmpty() {
+            return !it.hasNext();
+        }
+
+        @Override
+        public final void clear() {
+            // nothing to do
+        }
+
+        @Override
+        public final void request(long n) {
+            if (SubscriptionHelper.validateRequest(n)) {
+                if (BackpressureHelper.add(this, n) == 0L) {
+                    if (n == Long.MAX_VALUE) {
+                        fastPath();
+                    } else {
+                        slowPath(n);
+                    }
+                }
             }
-            long r = n;
-            long r0 = n;
-            final Subscriber<? super T> subscriber = this.subscriber;
-            final Iterator<? extends T> it = this.it;
+        }
+        
+
+        @Override
+        public final void cancel() {
+            cancelled = true;
+        }
+        
+        abstract void fastPath();
+        
+        abstract void slowPath(long r);
+    }
+    
+    static final class IteratorSubscription<T> extends BaseRangeSubscription<T> {
+
+        /** */
+        private static final long serialVersionUID = -6022804456014692607L;
+
+        final Subscriber<? super T> actual;
+        
+        public IteratorSubscription(Subscriber<? super T> actual, Iterator<? extends T> it) {
+            super(it);
+            this.actual = actual;
+        }
+
+        @Override
+        void fastPath() {
+            Iterator<? extends T> it = this.it;
+            Subscriber<? super T> a = actual;
             for (;;) {
                 if (cancelled) {
                     return;
                 }
+                
+                T t = it.next();
 
-                long e = 0L;
-                while (r != 0L) {
-                    T v;
-                    try {
-                        v = it.next();
-                    } catch (Throwable ex) {
-                        subscriber.onError(ex);
-                        return;
+                if (cancelled) {
+                    return;
+                }
+
+                if (t == null) {
+                    a.onError(new NullPointerException("Iterator.next() returned a null value"));
+                    return;
+                } else {
+                    a.onNext(t);
+                }
+
+                if (cancelled) {
+                    return;
+                }
+
+                if (!it.hasNext()) {
+                    if (!cancelled) {
+                        a.onComplete();
                     }
-                    
-                    if (v == null) {
-                        subscriber.onError(new NullPointerException("Iterator returned a null element"));
-                        return;
-                    }
-                    
-                    subscriber.onNext(v);
+                    return;
+                }
+            }
+        }
+
+        @Override
+        void slowPath(long r) {
+            long e = 0L;
+            Iterator<? extends T> it = this.it;
+            Subscriber<? super T> a = actual;
+            
+            for (;;) {
+                
+                while (e != r) {
                     
                     if (cancelled) {
                         return;
                     }
                     
-                    boolean hasNext;
-                    try {
-                        hasNext = it.hasNext();
-                    } catch (Throwable ex) {
-                        subscriber.onError(ex);
+                    T t = it.next();
+
+                    if (cancelled) {
                         return;
                     }
-                    if (!hasNext) {
-                        subscriber.onComplete();
+
+                    if (t == null) {
+                        a.onError(new NullPointerException("Iterator.next() returned a null value"));
+                        return;
+                    } else {
+                        a.onNext(t);
+                    }
+                    
+                    if (cancelled) {
+                        return;
+                    }
+
+                    if (!it.hasNext()) {
+                        if (!cancelled) {
+                            a.onComplete();
+                        }
                         return;
                     }
                     
-                    r--;
-                    e--;
+                    e++;
                 }
-                if (e != 0L && r0 != Long.MAX_VALUE) {
-                    r = addAndGet(e);
-                }
-                if (r == 0L) {
-                    break;
+                
+                r = get();
+                if (e == r) {
+                    
+                    if (cancelled) {
+                        return;
+                    }
+
+                    if (!it.hasNext()) {
+                        if (!cancelled) {
+                            a.onComplete();
+                        }
+                        return;
+                    }
+                    
+                    r = addAndGet(-e);
+                    if (r == 0L) {
+                        return;
+                    }
+                    e = 0L;
                 }
             }
         }
-        @Override
-        public void cancel() {
-            cancelled = true;
+        
+    }
+    
+    static final class IteratorConditionalSubscription<T> extends BaseRangeSubscription<T> {
+
+        /** */
+        private static final long serialVersionUID = -6022804456014692607L;
+
+        final ConditionalSubscriber<? super T> actual;
+        
+        public IteratorConditionalSubscription(ConditionalSubscriber<? super T> actual, Iterator<? extends T> it) {
+            super(it);
+            this.actual = actual;
         }
+
+        @Override
+        void fastPath() {
+            Iterator<? extends T> it = this.it;
+            ConditionalSubscriber<? super T> a = actual;
+            for (;;) {
+                if (cancelled) {
+                    return;
+                }
+                
+                T t = it.next();
+
+                if (cancelled) {
+                    return;
+                }
+
+                if (t == null) {
+                    a.onError(new NullPointerException("Iterator.next() returned a null value"));
+                    return;
+                } else {
+                    a.tryOnNext(t);
+                }
+
+                if (cancelled) {
+                    return;
+                }
+
+                if (!it.hasNext()) {
+                    if (!cancelled) {
+                        a.onComplete();
+                    }
+                    return;
+                }
+            }
+        }
+
+        @Override
+        void slowPath(long r) {
+            long e = 0L;
+            Iterator<? extends T> it = this.it;
+            ConditionalSubscriber<? super T> a = actual;
+            
+            for (;;) {
+                
+                while (e != r) {
+                    
+                    if (cancelled) {
+                        return;
+                    }
+                    
+                    T t = it.next();
+
+                    if (cancelled) {
+                        return;
+                    }
+
+                    boolean b;
+                    if (t == null) {
+                        a.onError(new NullPointerException("Iterator.next() returned a null value"));
+                        return;
+                    } else {
+                        b = a.tryOnNext(t);
+                    }
+                    
+                    if (cancelled) {
+                        return;
+                    }
+
+                    if (!it.hasNext()) {
+                        if (!cancelled) {
+                            a.onComplete();
+                        }
+                        return;
+                    }
+                    
+                    if (b) {
+                        e++;
+                    }
+                }
+                
+                r = get();
+                if (e == r) {
+                    
+                    if (cancelled) {
+                        return;
+                    }
+
+                    if (!it.hasNext()) {
+                        if (!cancelled) {
+                            a.onComplete();
+                        }
+                        return;
+                    }
+                    
+                    r = addAndGet(-e);
+                    if (r == 0L) {
+                        return;
+                    }
+                    e = 0L;
+                }
+            }
+        }
+        
     }
 }
---
+++ src/main/java/io/reactivex/internal/operators/flowable/FlowableHide.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2016 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.internal.operators.flowable;
+
+import org.reactivestreams.*;
+
+import io.reactivex.internal.subscriptions.SubscriptionHelper;
+
+/**
+ * Hides the identity of the wrapped Flowable and its Subscription.
+ * @param <T> the value type
+ * 
+ * @since 2.0
+ */
+public class FlowableHide<T> extends FlowableSource<T, T> {
+    
+    public FlowableHide(Publisher<T> source) {
+        super(source);
+    }
+
+    @Override
+    protected void subscribeActual(Subscriber<? super T> s) {
+        source.subscribe(new HideSubscriber<T>(s));
+    }
+
+    static final class HideSubscriber<T> implements Subscriber<T>, Subscription {
+        
+        final Subscriber<? super T> actual;
+        
+        Subscription s;
+
+        public HideSubscriber(Subscriber<? super T> actual) {
+            this.actual = actual;
+        }
+
+        @Override
+        public void request(long n) {
+            s.request(n);
+        }
+
+        @Override
+        public void cancel() {
+            s.cancel();
+        }
+
+        @Override
+        public void onSubscribe(Subscription s) {
+            if (SubscriptionHelper.validateSubscription(this.s, s)) {
+                this.s = s;
+                actual.onSubscribe(this);
+            }
+        }
+
+        @Override
+        public void onNext(T t) {
+            actual.onNext(t);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            actual.onError(t);
+        }
+
+        @Override
+        public void onComplete() {
+            actual.onComplete();
+        }
+    }
+}
