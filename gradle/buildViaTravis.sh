5e59bcf5ddc993ba6c7ab5dd29c7d8bad639a9a6
David Karnok <akarnokd@gmail.com>
2016-07-09 22:02:25 +0200

    1.x: no need to run gradle assembe by travis (#4183)
    * 1.x: no need to run gradle assembe by travis
    
    * Also update the sh with the parameter
    
    * +git fsck
    
    * full check
    
    * limit clone depth
    
    * Limit memory even further
    
    * disable PMD

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # This script will build the project.
 
+git fsck --full
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew -Prelease.useLastTag=true build
+  ./gradlew -Prelease.useLastTag=true build --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" build snapshot --stacktrace
@@ -12,5 +14,5 @@ elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" final --stacktrace
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew -Prelease.useLastTag=true build
+  ./gradlew -Prelease.useLastTag=true build --stacktrace
 fi
