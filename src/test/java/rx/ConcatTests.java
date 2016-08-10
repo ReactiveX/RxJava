d0b3bee3eea0a90384d44a03edd7b425a8a2ed77
Paul Woitaschek <woitaschek@posteo.de>
2016-08-10 18:33:38 +0200

    Added Observable.concat(Iterable) (#4330)
    * Added Observable.concat(Iterable)
    
    * Adjusted testConcatWithIterableOfObservable to really test iterables. Before it was testing the same as testConcatWithObservableOfObservable

--- src/test/java/rx/ConcatTests.java
+++ src/test/java/rx/ConcatTests.java
@@ -69,7 +69,7 @@ public class ConcatTests {
         @SuppressWarnings("unchecked")
         Iterable<Observable<String>> is = Arrays.asList(o1, o2, o3);
 
-        List<String> values = Observable.concat(Observable.from(is)).toList().toBlocking().single();
+        List<String> values = Observable.concat(is).toList().toBlocking().single();
 
         assertEquals("one", values.get(0));
         assertEquals("two", values.get(1));
