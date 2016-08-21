06eedeacb8d9a38b76a3d9ff877052d851ce1576
Jake Wharton <jakewharton@gmail.com>
2016-08-21 04:36:04 -0400

    Remove unused test methods from safe observer/subscriber. (#4387)

--- src/main/java/io/reactivex/subscribers/SafeSubscriber.java
+++ src/main/java/io/reactivex/subscribers/SafeSubscriber.java
@@ -192,8 +192,4 @@ public final class SafeSubscriber<T> implements Subscriber<T> {
             RxJavaPlugins.onError(e);
         }
     }
-    
-    /* test */ Subscriber<? super T> actual() {
-        return actual;
-    }
 }
