3279670ff13c81816d788ddea7f8a0390de553ea
akarnokd <akarnokd@gmail.com>
2016-09-08 14:11:56 +0200

    2.x: add Reactive-Streams TCK infrastructure and couple of tests

--- src/main/java/io/reactivex/internal/util/HalfSerializer.java
+++ src/main/java/io/reactivex/internal/util/HalfSerializer.java
@@ -30,6 +30,15 @@ public final class HalfSerializer {
         throw new IllegalStateException("No instances!");
     }
 
+    /**
+     * Emits the given value if possible and terminates if there was an onComplete or onError
+     * while emitting, drops the value otherwise.
+     * @param <T> the value type
+     * @param subscriber the target Subscriber to emit to
+     * @param value the value to emit
+     * @param wip the serialization work-in-progress counter/indicator
+     * @param error the holder of Throwables
+     */
     public static <T> void onNext(Subscriber<? super T> subscriber, T value,
             AtomicInteger wip, AtomicThrowable error) {
         if (wip.get() == 0 && wip.compareAndSet(0, 1)) {
@@ -45,6 +54,15 @@ public final class HalfSerializer {
         }
     }
 
+    /**
+     * Emits the given exception if possible or adds it to the given error container to
+     * be emitted by a concurrent onNext if one is running.
+     * Undeliverable exceptions are sent to the RxJavaPlugins.onError.
+     * @param subscriber the target Subscriber to emit to
+     * @param ex the Throwable to emit
+     * @param wip the serialization work-in-progress counter/indicator
+     * @param error the holder of Throwables
+     */
     public static void onError(Subscriber<?> subscriber, Throwable ex,
             AtomicInteger wip, AtomicThrowable error) {
         if (error.addThrowable(ex)) {
@@ -56,6 +74,14 @@ public final class HalfSerializer {
         }
     }
 
+
+    /**
+     * Emits an onComplete signal or an onError signal with the given error or indicates
+     * the concurrently running onNext should do that.
+     * @param subscriber the target Subscriber to emit to
+     * @param wip the serialization work-in-progress counter/indicator
+     * @param error the holder of Throwables
+     */
     public static void onComplete(Subscriber<?> subscriber, AtomicInteger wip, AtomicThrowable error) {
         if (wip.getAndIncrement() == 0) {
             Throwable ex = error.terminate();
@@ -67,39 +93,64 @@ public final class HalfSerializer {
         }
     }
 
-    public static <T> void onNext(Observer<? super T> subscriber, T value,
+    /**
+     * Emits the given value if possible and terminates if there was an onComplete or onError
+     * while emitting, drops the value otherwise.
+     * @param <T> the value type
+     * @param observer the target Observer to emit to
+     * @param value the value to emit
+     * @param wip the serialization work-in-progress counter/indicator
+     * @param error the holder of Throwables
+     */
+    public static <T> void onNext(Observer<? super T> observer, T value,
             AtomicInteger wip, AtomicThrowable error) {
         if (wip.get() == 0 && wip.compareAndSet(0, 1)) {
-            subscriber.onNext(value);
+            observer.onNext(value);
             if (wip.decrementAndGet() != 0) {
                 Throwable ex = error.terminate();
                 if (ex != null) {
-                    subscriber.onError(ex);
+                    observer.onError(ex);
                 } else {
-                    subscriber.onComplete();
+                    observer.onComplete();
                 }
             }
         }
     }
 
-    public static void onError(Observer<?> subscriber, Throwable ex,
+    /**
+     * Emits the given exception if possible or adds it to the given error container to
+     * be emitted by a concurrent onNext if one is running.
+     * Undeliverable exceptions are sent to the RxJavaPlugins.onError.
+     * @param observer the target Subscriber to emit to
+     * @param ex the Throwable to emit
+     * @param wip the serialization work-in-progress counter/indicator
+     * @param error the holder of Throwables
+     */
+    public static void onError(Observer<?> observer, Throwable ex,
             AtomicInteger wip, AtomicThrowable error) {
         if (error.addThrowable(ex)) {
             if (wip.getAndIncrement() == 0) {
-                subscriber.onError(error.terminate());
+                observer.onError(error.terminate());
             }
         } else {
             RxJavaPlugins.onError(ex);
         }
     }
 
-    public static void onComplete(Observer<?> subscriber, AtomicInteger wip, AtomicThrowable error) {
+    /**
+     * Emits an onComplete signal or an onError signal with the given error or indicates
+     * the concurrently running onNext should do that.
+     * @param observer the target Subscriber to emit to
+     * @param wip the serialization work-in-progress counter/indicator
+     * @param error the holder of Throwables
+     */
+    public static void onComplete(Observer<?> observer, AtomicInteger wip, AtomicThrowable error) {
         if (wip.getAndIncrement() == 0) {
             Throwable ex = error.terminate();
             if (ex != null) {
-                subscriber.onError(ex);
+                observer.onError(ex);
             } else {
-                subscriber.onComplete();
+                observer.onComplete();
             }
         }
     }
