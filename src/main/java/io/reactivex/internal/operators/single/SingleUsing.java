4f878d56988d572257309dad1a1d0c9fe601f3d8
David Karnok <akarnokd@gmail.com>
2016-09-09 00:38:06 +0200

    2.x: fix Single.using, add unit tests and coverage (#4516)

--- src/main/java/io/reactivex/internal/operators/single/SingleUsing.java
+++ src/main/java/io/reactivex/internal/operators/single/SingleUsing.java
@@ -14,12 +14,13 @@
 package io.reactivex.internal.operators.single;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicReference;
 
 import io.reactivex.*;
-import io.reactivex.disposables.*;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.exceptions.*;
 import io.reactivex.functions.*;
-import io.reactivex.internal.disposables.EmptyDisposable;
+import io.reactivex.internal.disposables.*;
 import io.reactivex.internal.functions.ObjectHelper;
 import io.reactivex.plugins.RxJavaPlugins;
 
@@ -31,15 +32,14 @@ public final class SingleUsing<T, U> extends Single<T> {
     final boolean eager;
 
     public SingleUsing(Callable<U> resourceSupplier,
-                       Function<? super U, ? extends SingleSource<? extends T>> singleFunction, Consumer<? super U> disposer,
+                       Function<? super U, ? extends SingleSource<? extends T>> singleFunction,
+                       Consumer<? super U> disposer,
                        boolean eager) {
         this.resourceSupplier = resourceSupplier;
         this.singleFunction = singleFunction;
         this.disposer = disposer;
         this.eager = eager;
     }
-
-
 
     @Override
     protected void subscribeActual(final SingleObserver<? super T> s) {
@@ -54,84 +54,142 @@ public final class SingleUsing<T, U> extends Single<T> {
             return;
         }
 
-        SingleSource<? extends T> s1;
+        SingleSource<? extends T> source;
 
         try {
-            s1 = ObjectHelper.requireNonNull(singleFunction.apply(resource), "The singleFunction returned a null SingleSource");
+            source = ObjectHelper.requireNonNull(singleFunction.apply(resource), "The singleFunction returned a null SingleSource");
         } catch (Throwable ex) {
             Exceptions.throwIfFatal(ex);
+
+            if (eager) {
+                try {
+                    disposer.accept(resource);
+                } catch (Throwable exc) {
+                    Exceptions.throwIfFatal(exc);
+                    ex = new CompositeException(ex, exc);
+                }
+            }
             EmptyDisposable.error(ex, s);
+            if (!eager) {
+                try {
+                    disposer.accept(resource);
+                } catch (Throwable exc) {
+                    Exceptions.throwIfFatal(exc);
+                    RxJavaPlugins.onError(exc);
+                }
+            }
             return;
         }
 
-        s1.subscribe(new SingleObserver<T>() {
-
-            @Override
-            public void onSubscribe(Disposable d) {
-                if (eager) {
-                    CompositeDisposable set = new CompositeDisposable();
-                    set.add(d);
-                    set.add(Disposables.fromRunnable(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                disposer.accept(resource);
-                            } catch (Throwable e) {
-                                Exceptions.throwIfFatal(e);
-                                RxJavaPlugins.onError(e);
-                            }
-                        }
-                    }));
-                    s.onSubscribe(set);
-                } else {
-                    s.onSubscribe(d);
-                }
-            }
-
-            @Override
-            public void onSuccess(T value) {
-                if (eager) {
-                    try {
-                        disposer.accept(resource);
-                    } catch (Throwable e) {
-                        Exceptions.throwIfFatal(e);
-                        s.onError(e);
-                        return;
-                    }
-                }
-                s.onSuccess(value);
-                if (!eager) {
-                    try {
-                        disposer.accept(resource);
-                    } catch (Throwable e) {
-                        Exceptions.throwIfFatal(e);
-                        RxJavaPlugins.onError(e);
-                    }
-                }
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                if (eager) {
-                    try {
-                        disposer.accept(resource);
-                    } catch (Throwable ex) {
-                        Exceptions.throwIfFatal(ex);
-                        e = new CompositeException(ex, e);
-                    }
-                }
-                s.onError(e);
-                if (!eager) {
-                    try {
-                        disposer.accept(resource);
-                    } catch (Throwable ex) {
-                        Exceptions.throwIfFatal(ex);
-                        RxJavaPlugins.onError(ex);
-                    }
-                }
-            }
-
-        });
+        source.subscribe(new UsingSingleObserver<T, U>(s, resource, eager, disposer));
     }
 
+    static final class UsingSingleObserver<T, U> extends
+    AtomicReference<Object> implements SingleObserver<T>, Disposable {
+        /** */
+        private static final long serialVersionUID = -5331524057054083935L;
+
+        final SingleObserver<? super T> actual;
+
+        final Consumer<? super U> disposer;
+
+        final boolean eager;
+
+        Disposable d;
+
+        public UsingSingleObserver(SingleObserver<? super T> actual, U resource, boolean eager,
+                Consumer<? super U> disposer) {
+            super(resource);
+            this.actual = actual;
+            this.eager = eager;
+            this.disposer = disposer;
+        }
+
+        @Override
+        public void dispose() {
+            d.dispose();
+            d = DisposableHelper.DISPOSED;
+            disposeAfter();
+        }
+
+        @Override
+        public boolean isDisposed() {
+            return d.isDisposed();
+        }
+
+        @Override
+        public void onSubscribe(Disposable d) {
+            if (DisposableHelper.validate(this.d, d)) {
+                this.d = d;
+
+                actual.onSubscribe(this);
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onSuccess(T value) {
+            d = DisposableHelper.DISPOSED;
+
+            if (eager) {
+                Object u = getAndSet(this);
+                if (u != this) {
+                    try {
+                        disposer.accept((U)u);
+                    } catch (Throwable ex) {
+                        Exceptions.throwIfFatal(ex);
+                        actual.onError(ex);
+                        return;
+                    }
+                } else {
+                    return;
+                }
+            }
+
+            actual.onSuccess(value);
+
+            if (!eager) {
+                disposeAfter();
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void onError(Throwable e) {
+            d = DisposableHelper.DISPOSED;
+
+            if (eager) {
+                Object u = getAndSet(this);
+                if (u != this) {
+                    try {
+                        disposer.accept((U)u);
+                    } catch (Throwable ex) {
+                        Exceptions.throwIfFatal(ex);
+                        e = new CompositeException(e, ex);
+                    }
+                } else {
+                    return;
+                }
+            }
+
+            actual.onError(e);
+
+            if (!eager) {
+                disposeAfter();
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        void disposeAfter() {
+            Object u = getAndSet(this);
+            if (u != this) {
+                try {
+                    disposer.accept((U)u);
+                } catch (Throwable ex) {
+                    Exceptions.throwIfFatal(ex);
+                    RxJavaPlugins.onError(ex);
+                }
+            }
+        }
+    }
 }
---
+++ src/test/java/io/reactivex/internal/operators/single/SingleLiftTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2016 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.internal.operators.single;
+
+import org.junit.Test;
+
+import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
+
+public class SingleLiftTest {
+
+    @Test
+    public void normal() {
+
+        Single.just(1).lift(new SingleOperator<Integer, Integer>() {
+            @Override
+            public SingleObserver<Integer> apply(final SingleObserver<? super Integer> s) throws Exception {
+                return new SingleObserver<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        s.onSubscribe(d);
+                    }
+
+                    @Override
+                    public void onSuccess(Integer value) {
+                        s.onSuccess(value + 1);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        s.onError(e);
+                    }
+                };
+            }
+        })
+        .test()
+        .assertResult(2);
+    }
+}
