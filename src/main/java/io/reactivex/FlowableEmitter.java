813a0832d8e5aaa56f8e112dc0119cd85135073c
Niklas Baudy <niklas.baudy@vanniktech.de>
2016-09-08 14:48:46 +0200

    Fix one place

--- src/main/java/io/reactivex/FlowableEmitter.java
+++ src/main/java/io/reactivex/FlowableEmitter.java
@@ -17,7 +17,7 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Cancellable;
 
 /**
- * Abstraction over an RxJava {@link org.reactivestreams.Subscriber} that allows associating
+ * Abstraction over a {@link org.reactivestreams.Subscriber} that allows associating
  * a resource with it and exposes the current number of downstream
  * requested amount.
  * <p>
