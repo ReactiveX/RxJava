10f2410269ef10d216e27417fe3dc01b6306178d
Prat Tanapaisankit <prat@meetup.com>
2016-09-28 22:57:02 -0400

    2.x: Add Completable.fromRunnable()

---
+++ src/main/java/io/reactivex/internal/operators/completable/CompletableFromRunnable.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2016 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.internal.operators.completable;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.Disposables;
+import io.reactivex.exceptions.Exceptions;
+
+public final class CompletableFromRunnable extends Completable {
+
+    final Runnable runnable;
+
+    public CompletableFromRunnable(Runnable runnable) {
+        this.runnable = runnable;
+    }
+
+    @Override
+    protected void subscribeActual(CompletableObserver s) {
+        Disposable d = Disposables.empty();
+        s.onSubscribe(d);
+        try {
+            runnable.run();
+        } catch (Throwable e) {
+            Exceptions.throwIfFatal(e);
+            if (!d.isDisposed()) {
+                s.onError(e);
+            }
+            return;
+        }
+        if (!d.isDisposed()) {
+            s.onComplete();
+        }
+    }
+}
