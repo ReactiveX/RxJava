d9d1b7ec784964964092bf1a9444eed10b4ac5ef
David Karnok <akarnokd@gmail.com>
2016-10-06 22:55:18 +0200

    2.x: fix flatMapX calling SpscLinkedArrayQueue.offer concurrently (#4678)

--- src/main/java/io/reactivex/internal/operators/flowable/FlowableFlatMapMaybe.java
+++ src/main/java/io/reactivex/internal/operators/flowable/FlowableFlatMapMaybe.java
@@ -191,14 +191,18 @@ public final class FlowableFlatMapMaybe<T, R> extends AbstractFlowableWithUpstre
                     }
                 } else {
                     SpscLinkedArrayQueue<R> q = getOrCreateQueue();
-                    q.offer(value);
+                    synchronized (q) {
+                        q.offer(value);
+                    }
                 }
                 if (decrementAndGet() == 0) {
                     return;
                 }
             } else {
                 SpscLinkedArrayQueue<R> q = getOrCreateQueue();
-                q.offer(value);
+                synchronized (q) {
+                    q.offer(value);
+                }
                 active.decrementAndGet();
                 if (getAndIncrement() != 0) {
                     return;
