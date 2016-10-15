d2cea35d2e02bc77791c1ade1abc0cedacca4366
Dávid Karnok <akarnokd@gmail.com>
2016-10-16 00:54:49 +0200

    Add missing header

--- src/test/java/io/reactivex/internal/operators/maybe/MaybeConcatArrayTest.java
+++ src/test/java/io/reactivex/internal/operators/maybe/MaybeConcatArrayTest.java
@@ -1,9 +1,16 @@
 /**
- * Concatenate values of each MaybeSource provided in an array and delays
- * any errors till the very end.
+ * Copyright 2016 Netflix, Inc.
  *
- * @param <T> the value type
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
  */
+
 package io.reactivex.internal.operators.maybe;
 
 import java.io.IOException;
