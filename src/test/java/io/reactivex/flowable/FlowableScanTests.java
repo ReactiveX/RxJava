75e9bfa3bd1e0c084d20ed671d33d05d5ff0f8ab
Dave Moten <davidmoten@gmail.com>
2016-12-04 03:28:31 +1100

    FlowableScanSeed - prevent post-terminal events (#4899)

--- src/test/java/io/reactivex/flowable/FlowableScanTests.java
+++ src/test/java/io/reactivex/flowable/FlowableScanTests.java
@@ -13,10 +13,16 @@
 
 package io.reactivex.flowable;
 
-import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import io.reactivex.Flowable;
 import io.reactivex.flowable.FlowableEventStream.Event;
 import io.reactivex.functions.*;
 
@@ -40,5 +46,84 @@ public class FlowableScanTests {
                 System.out.println(v);
             }
         });
+    }
+    
+    @Test
+    public void testFlowableScanSeedDoesNotEmitErrorTwiceIfScanFunctionThrows() {
+        final RuntimeException e = new RuntimeException();
+        Burst.item(1).error(e).scan(0, new BiFunction<Integer, Integer, Integer>() {
+
+            @Override
+            public Integer apply(Integer n1, Integer n2) throws Exception {
+                throw e;
+            }})
+          .test()
+          .assertNoValues()
+          .assertError(e);
+    }
+    
+    @Test
+    public void testFlowableScanSeedDoesNotEmitTerminalEventTwiceIfScanFunctionThrows() {
+        final RuntimeException e = new RuntimeException();
+        Burst.item(1).create().scan(0, new BiFunction<Integer, Integer, Integer>() {
+
+            @Override
+            public Integer apply(Integer n1, Integer n2) throws Exception {
+                throw e;
+            }})
+          .test()
+          .assertNoValues()
+          .assertError(e);
+    }
+    
+    @Test
+    public void testFlowableScanSeedDoesNotProcessOnNextAfterTerminalEventIfScanFunctionThrows() {
+        final RuntimeException e = new RuntimeException();
+        final AtomicInteger count = new AtomicInteger();
+        Burst.items(1, 2).create().scan(0, new BiFunction<Integer, Integer, Integer>() {
+
+            @Override
+            public Integer apply(Integer n1, Integer n2) throws Exception {
+                count.incrementAndGet();
+                throw e;
+            }})
+          .test()
+          .assertNoValues()
+          .assertError(e);
+        assertEquals(1, count.get());
+    }
+    
+    @Test
+    public void testFlowableScanSeedCompletesNormally() {
+        Flowable.just(1,2,3).scan(0, new BiFunction<Integer, Integer, Integer>() {
+
+            @Override
+            public Integer apply(Integer t1, Integer t2) throws Exception {
+                return t1 + t2;
+            }})
+          .test()
+          .assertValues(0, 1, 3, 6)
+          .assertComplete();
+    }
+    
+    @Test
+    public void testFlowableScanSeedWhenScanSeedProviderThrows() {
+        final RuntimeException e = new RuntimeException();
+        Flowable.just(1,2,3).scanWith(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                throw e;
+            }
+        },
+            new BiFunction<Integer, Integer, Integer>() {
+
+                @Override
+                public Integer apply(Integer t1, Integer t2) throws Exception {
+                    return t1 + t2;
+                }
+           })
+          .test()
+          .assertError(e)
+          .assertNoValues();
     }
 }
