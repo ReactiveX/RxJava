d3455d0c9d57d522c31b5c25af83e8f2b8df12b6
Ben Christensen <benjchristensen@gmail.com>
2017-01-09 15:21:37 -0800

    Update Copyright to 'RxJava Contributors' (#4980)
    See https://github.com/ReactiveX/RxJava/issues/4978 for more information.

--- src/test/java/io/reactivex/internal/util/TestingHelper.java
+++ src/test/java/io/reactivex/internal/util/TestingHelper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Netflix, Inc.
+ * Copyright (c) 2016-present, RxJava Contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
