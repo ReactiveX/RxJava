9d2b9807f9402e5b637f01869b26017cfaec79e3
Ben Christensen <benjchristensen@fb.com>
2017-01-09 13:21:59 -0800

    Update Copyright to 'RxJava Contributors'
    See https://github.com/ReactiveX/RxJava/issues/4978 for more information.

--- src/main/java/io/reactivex/internal/subscriptions/SubscriptionArbiter.java
+++ src/main/java/io/reactivex/internal/subscriptions/SubscriptionArbiter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Netflix, Inc.
+ * Copyright (c) 2016-present, RxJava Contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
@@ -13,7 +13,7 @@
 
 package io.reactivex.internal.subscriptions;
 /**
- * Copyright 2016 Netflix, Inc.
+ * Copyright (c) 2016-present, RxJava Contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
