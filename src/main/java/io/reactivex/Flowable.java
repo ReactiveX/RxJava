cf6f5b887bdd0f2e568c600a94bb931d8969c581
Leonardo Lima <leonardortlima@gmail.com>
2017-07-09 19:32:15 -0300

    Fix Javadoc for Flowable and Observable reduce. (#5476)

--- src/main/java/io/reactivex/Flowable.java
+++ src/main/java/io/reactivex/Flowable.java
@@ -10745,7 +10745,7 @@ public abstract class Flowable<T> implements Publisher<T> {
     }
 
     /**
-     * Returns a Flowable that applies a specified accumulator function to the first item emitted by a source
+     * Returns a Single that applies a specified accumulator function to the first item emitted by a source
      * Publisher and a specified seed value, then feeds the result of that function along with the second item
      * emitted by a Publisher into the same function, and so on until all items have been emitted by the
      * source Publisher, emitting the final result from the final call to your function as its sole item.
