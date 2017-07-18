81fe956dc0100dc5b0b5101669a8c326f930f93e
David Karnok <akarnokd@gmail.com>
2017-07-18 10:42:48 +0200

    2.x: fix ReplayProcessor backpressure and NotificationLite emission bug (#5493)
    * 2.x: fix ReplayProcessor backpressure and NotificationLite emission bug
    
    * Restore coverage

--- src/main/java/io/reactivex/internal/operators/flowable/FlowableTimeoutTimed.java
+++ src/main/java/io/reactivex/internal/operators/flowable/FlowableTimeoutTimed.java
@@ -14,14 +14,12 @@
 package io.reactivex.internal.operators.flowable;
 
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.reactivestreams.*;
 
 import io.reactivex.*;
 import io.reactivex.Scheduler.Worker;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.internal.disposables.DisposableHelper;
 import io.reactivex.internal.subscribers.FullArbiterSubscriber;
 import io.reactivex.internal.subscriptions.*;
 import io.reactivex.plugins.RxJavaPlugins;
