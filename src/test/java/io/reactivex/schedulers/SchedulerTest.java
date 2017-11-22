a75a7f1f798cf97b017df24376d96466fe0708da
lguz <lukasz.guz@upaid.pl>
2017-11-22 08:42:34 +0100

    2.x: RxJavaPlugins unwrapRunnable

--- src/test/java/io/reactivex/schedulers/SchedulerTest.java
+++ src/test/java/io/reactivex/schedulers/SchedulerTest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2016-present, RxJava Contributors.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
  * the License for the specific language governing permissions and limitations under the License.
@@ -32,7 +32,7 @@ public class SchedulerTest {
 
     @Test
     public void defaultPeriodicTask() {
-        final int[] count = {0};
+        final int[] count = { 0 };
 
         TestScheduler scheduler = new TestScheduler();
 
@@ -76,7 +76,7 @@ public class SchedulerTest {
 
     @Test
     public void disposePeriodicDirect() {
-        final int[] count = {0};
+        final int[] count = { 0 };
 
         TestScheduler scheduler = new TestScheduler();
 
@@ -100,7 +100,7 @@ public class SchedulerTest {
 
     @Test
     public void scheduleDirect() {
-        final int[] count = {0};
+        final int[] count = { 0 };
 
         TestScheduler scheduler = new TestScheduler();
 
@@ -120,7 +120,7 @@ public class SchedulerTest {
 
     @Test
     public void disposeSelfPeriodicDirect() {
-        final int[] count = {0};
+        final int[] count = { 0 };
 
         TestScheduler scheduler = new TestScheduler();
 
