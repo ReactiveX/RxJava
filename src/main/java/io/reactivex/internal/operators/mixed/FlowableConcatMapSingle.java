44fb7cd2dfbc0127e0734111431e5a693cafac6b
David Karnok <akarnokd@gmail.com>
2018-03-01 19:46:57 +0100

    2.x: Add Flowable.concatMap{Maybe,Single}{DelayError} operators (#5872)
    * 2.x: Add Flowable.concatMap{Maybe,Single}{DelayError} operators
    
    * Fix typo in the JavaDocs
    
    * Fix the JavaDoc wording of concatMapSingle

---
+++ src/main/java/io/reactivex/internal/operators/mixed/FlowableConcatMapSingle.java
@@ -0,0 +1,332 @@
+/**
+ * Copyright (c) 2016-present, RxJava Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.internal.operators.mixed;
+
+import java.util.concurrent.atomic.*;
+
+import org.reactivestreams.*;
+
+import io.reactivex.*;
+import io.reactivex.annotations.Experimental;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.exceptions.*;
+import io.reactivex.functions.Function;
+import io.reactivex.internal.disposables.DisposableHelper;
+import io.reactivex.internal.functions.ObjectHelper;
+import io.reactivex.internal.fuseable.SimplePlainQueue;
+import io.reactivex.internal.queue.SpscArrayQueue;
+import io.reactivex.internal.subscriptions.SubscriptionHelper;
+import io.reactivex.internal.util.*;
+import io.reactivex.plugins.RxJavaPlugins;
+
+/**
+ * Maps each upstream item into a {@link SingleSource}, subscribes to them one after the other terminates
+ * and relays their success values, optionally delaying any errors till the main and inner sources
+ * terminate.
+ *
+ * @param <T> the upstream element type
+ * @param <R> the output element type
+ *
+ * @since 2.1.11 - experimental
+ */
+@Experimental
+public final class FlowableConcatMapSingle<T, R> extends Flowable<R> {
+
+    final Flowable<T> source;
+
+    final Function<? super T, ? extends SingleSource<? extends R>> mapper;
+
+    final ErrorMode errorMode;
+
+    final int prefetch;
+
+    public FlowableConcatMapSingle(Flowable<T> source,
+            Function<? super T, ? extends SingleSource<? extends R>> mapper,
+                    ErrorMode errorMode, int prefetch) {
+        this.source = source;
+        this.mapper = mapper;
+        this.errorMode = errorMode;
+        this.prefetch = prefetch;
+    }
+
+    @Override
+    protected void subscribeActual(Subscriber<? super R> s) {
+        source.subscribe(new ConcatMapSingleSubscriber<T, R>(s, mapper, prefetch, errorMode));
+    }
+
+    static final class ConcatMapSingleSubscriber<T, R>
+    extends AtomicInteger
+    implements FlowableSubscriber<T>, Subscription {
+
+        private static final long serialVersionUID = -9140123220065488293L;
+
+        final Subscriber<? super R> downstream;
+
+        final Function<? super T, ? extends SingleSource<? extends R>> mapper;
+
+        final int prefetch;
+
+        final AtomicLong requested;
+
+        final AtomicThrowable errors;
+
+        final ConcatMapSingleObserver<R> inner;
+
+        final SimplePlainQueue<T> queue;
+
+        final ErrorMode errorMode;
+
+        Subscription upstream;
+
+        volatile boolean done;
+
+        volatile boolean cancelled;
+
+        long emitted;
+
+        int consumed;
+
+        R item;
+
+        volatile int state;
+
+        /** No inner SingleSource is running. */
+        static final int STATE_INACTIVE = 0;
+        /** An inner SingleSource is running but there are no results yet. */
+        static final int STATE_ACTIVE = 1;
+        /** The inner SingleSource succeeded with a value in {@link #item}. */
+        static final int STATE_RESULT_VALUE = 2;
+
+        ConcatMapSingleSubscriber(Subscriber<? super R> downstream,
+                Function<? super T, ? extends SingleSource<? extends R>> mapper,
+                        int prefetch, ErrorMode errorMode) {
+            this.downstream = downstream;
+            this.mapper = mapper;
+            this.prefetch = prefetch;
+            this.errorMode = errorMode;
+            this.requested = new AtomicLong();
+            this.errors = new AtomicThrowable();
+            this.inner = new ConcatMapSingleObserver<R>(this);
+            this.queue = new SpscArrayQueue<T>(prefetch);
+        }
+
+        @Override
+        public void onSubscribe(Subscription s) {
+            if (SubscriptionHelper.validate(upstream, s)) {
+                upstream = s;
+                downstream.onSubscribe(this);
+                s.request(prefetch);
+            }
+        }
+
+        @Override
+        public void onNext(T t) {
+            if (!queue.offer(t)) {
+                upstream.cancel();
+                onError(new MissingBackpressureException("queue full?!"));
+                return;
+            }
+            drain();
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            if (errors.addThrowable(t)) {
+                if (errorMode == ErrorMode.IMMEDIATE) {
+                    inner.dispose();
+                }
+                done = true;
+                drain();
+            } else {
+                RxJavaPlugins.onError(t);
+            }
+        }
+
+        @Override
+        public void onComplete() {
+            done = true;
+            drain();
+        }
+
+        @Override
+        public void request(long n) {
+            BackpressureHelper.add(requested, n);
+            drain();
+        }
+
+        @Override
+        public void cancel() {
+            cancelled = true;
+            upstream.cancel();
+            inner.dispose();
+            if (getAndIncrement() != 0) {
+                queue.clear();
+                item = null;
+            }
+        }
+
+        void innerSuccess(R item) {
+            this.item = item;
+            this.state = STATE_RESULT_VALUE;
+            drain();
+        }
+
+        void innerError(Throwable ex) {
+            if (errors.addThrowable(ex)) {
+                if (errorMode != ErrorMode.END) {
+                    upstream.cancel();
+                }
+                this.state = STATE_INACTIVE;
+                drain();
+            } else {
+                RxJavaPlugins.onError(ex);
+            }
+        }
+
+        void drain() {
+            if (getAndIncrement() != 0) {
+                return;
+            }
+
+            int missed = 1;
+            Subscriber<? super R> downstream = this.downstream;
+            ErrorMode errorMode = this.errorMode;
+            SimplePlainQueue<T> queue = this.queue;
+            AtomicThrowable errors = this.errors;
+            AtomicLong requested = this.requested;
+            int limit = prefetch - (prefetch >> 1);
+
+            for (;;) {
+
+                for (;;) {
+                    if (cancelled) {
+                        queue.clear();
+                        item = null;
+                    }
+
+                    int s = state;
+
+                    if (errors.get() != null) {
+                        if (errorMode == ErrorMode.IMMEDIATE
+                                || (errorMode == ErrorMode.BOUNDARY && s == STATE_INACTIVE)) {
+                            queue.clear();
+                            item = null;
+                            Throwable ex = errors.terminate();
+                            downstream.onError(ex);
+                            return;
+                        }
+                    }
+
+                    if (s == STATE_INACTIVE) {
+                        boolean d = done;
+                        T v = queue.poll();
+                        boolean empty = v == null;
+
+                        if (d && empty) {
+                            Throwable ex = errors.terminate();
+                            if (ex == null) {
+                                downstream.onComplete();
+                            } else {
+                                downstream.onError(ex);
+                            }
+                            return;
+                        }
+
+                        if (empty) {
+                            break;
+                        }
+
+                        int c = consumed + 1;
+                        if (c == limit) {
+                            consumed = 0;
+                            upstream.request(limit);
+                        } else {
+                            consumed = c;
+                        }
+
+                        SingleSource<? extends R> ms;
+
+                        try {
+                            ms = ObjectHelper.requireNonNull(mapper.apply(v), "The mapper returned a null SingleSource");
+                        } catch (Throwable ex) {
+                            Exceptions.throwIfFatal(ex);
+                            upstream.cancel();
+                            queue.clear();
+                            errors.addThrowable(ex);
+                            ex = errors.terminate();
+                            downstream.onError(ex);
+                            return;
+                        }
+
+                        state = STATE_ACTIVE;
+                        ms.subscribe(inner);
+                        break;
+                    } else if (s == STATE_RESULT_VALUE) {
+                        long e = emitted;
+                        if (e != requested.get()) {
+                            R w = item;
+                            item = null;
+
+                            downstream.onNext(w);
+
+                            emitted = e + 1;
+                            state = STATE_INACTIVE;
+                        } else {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                missed = addAndGet(-missed);
+                if (missed == 0) {
+                    break;
+                }
+            }
+        }
+
+        static final class ConcatMapSingleObserver<R>
+        extends AtomicReference<Disposable>
+        implements SingleObserver<R> {
+
+            private static final long serialVersionUID = -3051469169682093892L;
+
+            final ConcatMapSingleSubscriber<?, R> parent;
+
+            ConcatMapSingleObserver(ConcatMapSingleSubscriber<?, R> parent) {
+                this.parent = parent;
+            }
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                DisposableHelper.replace(this, d);
+            }
+
+            @Override
+            public void onSuccess(R t) {
+                parent.innerSuccess(t);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                parent.innerError(e);
+            }
+
+            void dispose() {
+                DisposableHelper.dispose(this);
+            }
+        }
+    }
+}
---
+++ src/test/java/io/reactivex/internal/operators/mixed/FlowableConcatMapMaybeTest.java
@@ -0,0 +1,371 @@
+/**
+ * Copyright (c) 2016-present, RxJava Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.internal.operators.mixed;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.Test;
+import org.reactivestreams.Subscriber;
+
+import io.reactivex.*;
+import io.reactivex.disposables.Disposables;
+import io.reactivex.exceptions.*;
+import io.reactivex.functions.*;
+import io.reactivex.internal.functions.Functions;
+import io.reactivex.internal.subscriptions.BooleanSubscription;
+import io.reactivex.plugins.RxJavaPlugins;
+import io.reactivex.processors.PublishProcessor;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.MaybeSubject;
+import io.reactivex.subscribers.TestSubscriber;
+
+public class FlowableConcatMapMaybeTest {
+
+    @Test
+    public void simple() {
+        Flowable.range(1, 5)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                return Maybe.just(v);
+            }
+        })
+        .test()
+        .assertResult(1, 2, 3, 4, 5);
+    }
+
+    @Test
+    public void simpleLong() {
+        Flowable.range(1, 1024)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                return Maybe.just(v);
+            }
+        }, 32)
+        .test()
+        .assertValueCount(1024)
+        .assertNoErrors()
+        .assertComplete();
+    }
+
+    @Test
+    public void backpressure() {
+        TestSubscriber<Integer> ts = Flowable.range(1, 1024)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                return Maybe.just(v);
+            }
+        }, 32)
+        .test(0);
+
+        for (int i = 1; i <= 1024; i++) {
+            ts.assertValueCount(i - 1)
+            .assertNoErrors()
+            .assertNotComplete()
+            .requestMore(1)
+            .assertValueCount(i)
+            .assertNoErrors();
+        }
+
+        ts.assertComplete();
+    }
+
+    @Test
+    public void empty() {
+        Flowable.range(1, 10)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                return Maybe.empty();
+            }
+        })
+        .test()
+        .assertResult();
+    }
+
+    @Test
+    public void mixed() {
+        Flowable.range(1, 10)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                if (v % 2 == 0) {
+                    return Maybe.just(v);
+                }
+                return Maybe.empty();
+            }
+        })
+        .test()
+        .assertResult(2, 4, 6, 8, 10);
+    }
+
+    @Test
+    public void mixedLong() {
+        Flowable.range(1, 1024)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                if (v % 2 == 0) {
+                    return Maybe.just(v).subscribeOn(Schedulers.computation());
+                }
+                return Maybe.<Integer>empty().subscribeOn(Schedulers.computation());
+            }
+        })
+        .test()
+        .awaitDone(5, TimeUnit.SECONDS)
+        .assertValueCount(512)
+        .assertNoErrors()
+        .assertComplete()
+        .assertOf(new Consumer<TestSubscriber<Integer>>() {
+            @Override
+            public void accept(TestSubscriber<Integer> ts) throws Exception {
+                for (int i = 0; i < 512; i ++) {
+                    ts.assertValueAt(i, (i + 1) * 2);
+                }
+            }
+        });
+    }
+
+    @Test
+    public void mainError() {
+        Flowable.error(new TestException())
+        .concatMapMaybe(Functions.justFunction(Maybe.just(1)))
+        .test()
+        .assertFailure(TestException.class);
+    }
+
+    @Test
+    public void innerError() {
+        Flowable.just(1)
+        .concatMapMaybe(Functions.justFunction(Maybe.error(new TestException())))
+        .test()
+        .assertFailure(TestException.class);
+    }
+
+    @Test
+    public void mainBoundaryErrorInnerSuccess() {
+        PublishProcessor<Integer> pp = PublishProcessor.create();
+        MaybeSubject<Integer> ms = MaybeSubject.create();
+
+        TestSubscriber<Integer> ts = pp.concatMapMaybeDelayError(Functions.justFunction(ms), false).test();
+
+        ts.assertEmpty();
+
+        pp.onNext(1);
+
+        assertTrue(ms.hasObservers());
+
+        pp.onError(new TestException());
+
+        assertTrue(ms.hasObservers());
+
+        ts.assertEmpty();
+
+        ms.onSuccess(1);
+
+        ts.assertFailure(TestException.class, 1);
+    }
+
+    @Test
+    public void mainBoundaryErrorInnerEmpty() {
+        PublishProcessor<Integer> pp = PublishProcessor.create();
+        MaybeSubject<Integer> ms = MaybeSubject.create();
+
+        TestSubscriber<Integer> ts = pp.concatMapMaybeDelayError(Functions.justFunction(ms), false).test();
+
+        ts.assertEmpty();
+
+        pp.onNext(1);
+
+        assertTrue(ms.hasObservers());
+
+        pp.onError(new TestException());
+
+        assertTrue(ms.hasObservers());
+
+        ts.assertEmpty();
+
+        ms.onComplete();
+
+        ts.assertFailure(TestException.class);
+    }
+
+    @Test
+    public void doubleOnSubscribe() {
+        TestHelper.checkDoubleOnSubscribeFlowable(
+                new Function<Flowable<Object>, Flowable<Object>>() {
+                    @Override
+                    public Flowable<Object> apply(Flowable<Object> f)
+                            throws Exception {
+                        return f.concatMapMaybeDelayError(
+                                Functions.justFunction(Maybe.empty()));
+                    }
+                }
+        );
+    }
+
+    @Test
+    public void queueOverflow() {
+        List<Throwable> errors = TestHelper.trackPluginErrors();
+        try {
+            new Flowable<Integer>() {
+                @Override
+                protected void subscribeActual(Subscriber<? super Integer> s) {
+                    s.onSubscribe(new BooleanSubscription());
+                    s.onNext(1);
+                    s.onNext(2);
+                    s.onNext(3);
+                    s.onError(new TestException());
+                }
+            }
+            .concatMapMaybe(
+                    Functions.justFunction(Maybe.never()), 1
+            )
+            .test()
+            .assertFailure(MissingBackpressureException.class);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+        } finally {
+            RxJavaPlugins.reset();
+        }
+    }
+
+    @Test
+    public void limit() {
+        Flowable.range(1, 5)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                return Maybe.just(v);
+            }
+        })
+        .limit(3)
+        .test()
+        .assertResult(1, 2, 3);
+    }
+
+    @Test
+    public void cancel() {
+        Flowable.range(1, 5)
+        .concatMapMaybe(new Function<Integer, MaybeSource<Integer>>() {
+            @Override
+            public MaybeSource<Integer> apply(Integer v)
+                    throws Exception {
+                return Maybe.just(v);
+            }
+        })
+        .test(3)
+        .assertValues(1, 2, 3)
+        .assertNoErrors()
+        .assertNotComplete()
+        .cancel();
+    }
+
+    @Test
+    public void innerErrorAfterMainError() {
+        List<Throwable> errors = TestHelper.trackPluginErrors();
+        try {
+            final PublishProcessor<Integer> pp = PublishProcessor.create();
+
+            final AtomicReference<MaybeObserver<? super Integer>> obs = new AtomicReference<MaybeObserver<? super Integer>>();
+
+            TestSubscriber<Integer> ts = pp.concatMapMaybe(
+                    new Function<Integer, MaybeSource<Integer>>() {
+                        @Override
+                        public MaybeSource<Integer> apply(Integer v)
+                                throws Exception {
+                            return new Maybe<Integer>() {
+                                    @Override
+                                    protected void subscribeActual(
+                                            MaybeObserver<? super Integer> observer) {
+                                        observer.onSubscribe(Disposables.empty());
+                                        obs.set(observer);
+                                    }
+                            };
+                        }
+                    }
+            ).test();
+
+            pp.onNext(1);
+
+            pp.onError(new TestException("outer"));
+            obs.get().onError(new TestException("inner"));
+
+            ts.assertFailureAndMessage(TestException.class, "outer");
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class, "inner");
+        } finally {
+            RxJavaPlugins.reset();
+        }
+    }
+
+    @Test
+    public void delayAllErrors() {
+        Flowable.range(1, 5)
+        .concatMapMaybeDelayError(new Function<Integer, MaybeSource<? extends Object>>() {
+            @Override
+            public MaybeSource<? extends Object> apply(Integer v)
+                    throws Exception {
+                return Maybe.error(new TestException());
+            }
+        })
+        .test()
+        .assertFailure(CompositeException.class)
+        .assertOf(new Consumer<TestSubscriber<Object>>() {
+            @Override
+            public void accept(TestSubscriber<Object> ts) throws Exception {
+                CompositeException ce = (CompositeException)ts.errors().get(0);
+                assertEquals(5, ce.getExceptions().size());
+            }
+        });
+    }
+
+    @Test
+    public void mapperCrash() {
+        final PublishProcessor<Integer> pp = PublishProcessor.create();
+
+        TestSubscriber<Object> ts = pp
+        .concatMapMaybe(new Function<Integer, MaybeSource<? extends Object>>() {
+            @Override
+            public MaybeSource<? extends Object> apply(Integer v)
+                    throws Exception {
+                        throw new TestException();
+                    }
+        })
+        .test();
+
+        ts.assertEmpty();
+
+        assertTrue(pp.hasSubscribers());
+
+        pp.onNext(1);
+
+        ts.assertFailure(TestException.class);
+
+        assertFalse(pp.hasSubscribers());
+    }
+}
