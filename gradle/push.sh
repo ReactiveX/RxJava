953c0e2ab447ddaaee1279c8d1a7cdf32a297ee7
Dávid Karnok <akarnokd@gmail.com>
2018-05-05 23:43:49 +0200

    Fix script error, add logging

--- gradle/push.sh
+++ gradle/push.sh
@@ -14,7 +14,7 @@ targetRepo=github.com/ReactiveX/RxJava.git
 	exit 0
 fi
 
-// get the current build tag if any
+# get the current build tag if any
 buildTag="$TRAVIS_TAG"
 
 if [ "$buildTag" == ""]; then
@@ -36,12 +36,15 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 # setup the remote
+echo -e "Adding the target repository to git"
 git remote add origin-pages https://${GITHUB_TOKEN}@${targetRepo} > /dev/null 2>&1
 
 # stash changes due to chmod
+echo -e "Stashing any local non-ignored changes"
 git stash
 
 # get the gh-pages
+echo -e "Update branches and checking out gh-pages"
 git fetch --all
 git branch -a
 git checkout -b gh-pages origin-pages/gh-pages
@@ -56,44 +59,61 @@ if [ "$buildTag" != "" ]; then
 	# 1.) main javadoc
 	# ----------------
 	# remove the io subdir
+    echo -e "Removing javadoc/io" 
 	rm -r javadoc/io
+
 	# remove the html files
+    echo -e "Removing javadoc/*.html" 
 	rm javadoc/*.html
+
 	# copy the new doc
+    echo -e "Copying to javadoc/" 
 	yes | cp -rf ./build/docs/javadoc/ javadoc/
 	
 	# 2.) 2.x javadoc
 	# remove the io subdir
+    echo -e "Removing 2.x/javadoc/io" 
 	rm -r 2.x/javadoc/io
+
 	# remove the html files
+    echo -e "Removing 2.x/javadoc/*.html" 
 	rm 2.x/javadoc/*.html
+
 	# copy the new doc
+    echo -e "Copying to 2.x/javadoc/" 
 	yes | cp -rf ./build/docs/javadoc/ 2.x/javadoc/
 fi
 
 # 3.) create a version/snapshot specific copy of the docs
 # clear the existing tag
+echo -e "Removing to 2.x/javadoc/${buildTag}" 
 rm -r 2.x/javadoc/${buildTag}
 # copy the new doc
+echo -e "Copying to 2.x/javadoc/${buildTag}" 
 yes | cp -rf ./build/docs/javadoc/ 2.x/javadoc/${buildTag}/
 
 
 # stage all changed and new files
+echo -e "Staging new files" 
 git add *.html
 git add *.css
 git add *.js
 git add javadoc/package-list
 
 # remove tracked but deleted files
+echo -e "Removing deleted files"
 git add -u
 
 # commit all
+echo -e "commit Travis build: $TRAVIS_BUILD_NUMBER for $buildTag"
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER for $buildTag"
 
 
 # push it
+echo -e "Pushing back changes."
 #git push --quiet --set-upstream origin-pages gh-pages
 # just print the result for now
-find .
+find -name "*.html"
 
 # we are done
+echo -e "JavaDocs pushback complete."
