83d19b445b9480c06228b09a042e5fe1b66b8649
akarnokd <akarnokd@gmail.com>
2019-12-20 15:56:02 +0100

    Fix a wrong tag name

--- src/main/java/io/reactivex/rxjava3/processors/ReplayProcessor.java
+++ src/main/java/io/reactivex/rxjava3/processors/ReplayProcessor.java
@@ -104,7 +104,7 @@ import io.reactivex.rxjava3.plugins.RxJavaPlugins;
  *  <dt><b>Backpressure:</b></dt>
  *  <dd>This {@code ReplayProcessor} respects the individual backpressure behavior of its {@code Subscriber}s but
  *  does not coordinate their request amounts towards the upstream (because there might not be any) and
- *  consumes the upstream in an unbounded manner (requesting {@long Long#MAX_VALUE}).
+ *  consumes the upstream in an unbounded manner (requesting {@link Long#MAX_VALUE}).
  *  Note that {@code Subscriber}s receive a continuous sequence of values after they subscribed even
  *  if an individual item gets delayed due to backpressure.</dd>
  *  <dt><b>Scheduler:</b></dt>
