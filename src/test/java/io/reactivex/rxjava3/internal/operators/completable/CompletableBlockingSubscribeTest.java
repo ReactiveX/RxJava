e1a480c6eb5a39d7c3848720e35f573794112b4a
akarnokd <akarnokd@gmail.com>
2020-01-23 14:05:17 +0100

    3.x: Add Maybe/Single/Completable blockingSubscribe

---
+++ src/test/java/io/reactivex/rxjava3/internal/operators/completable/CompletableBlockingSubscribeTest.java
@@ -0,0 +1,331 @@
+/**
+ * Copyright (c) 2016-present, RxJava Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.rxjava3.internal.operators.completable;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.exceptions.TestException;
+import io.reactivex.rxjava3.functions.*;
+import io.reactivex.rxjava3.observers.TestObserver;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.testsupport.TestHelper;
+
+public class CompletableBlockingSubscribeTest {
+
+    @Test
+    public void noArgComplete() {
+        Completable.complete()
+        .blockingSubscribe();
+    }
+
+    @Test
+    public void noArgCompleteAsync() {
+        Completable.complete()
+        .delay(100, TimeUnit.MILLISECONDS)
+        .blockingSubscribe();
+    }
+
+    @Test
+    public void noArgError() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Completable.error(new TestException())
+            .blockingSubscribe();
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+        });
+    }
+
+    @Test
+    public void noArgErrorAsync() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Completable.error(new TestException())
+            .delay(100, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe();
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+        });
+    }
+
+    @Test
+    public void oneArgComplete() throws Throwable {
+        Action action = mock(Action.class);
+
+        Completable.complete()
+        .blockingSubscribe(action);
+
+        verify(action).run();
+    }
+
+    @Test
+    public void oneArgCompleteAsync() throws Throwable {
+        Action action = mock(Action.class);
+
+        Completable.complete()
+        .delay(50, TimeUnit.MILLISECONDS)
+        .blockingSubscribe(action);
+
+        verify(action).run();
+    }
+
+    @Test
+    public void oneArgCompleteFails() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action action = mock(Action.class);
+            doThrow(new TestException()).when(action).run();
+
+            Completable.complete()
+            .blockingSubscribe(action);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(action).run();
+        });
+    }
+
+    @Test
+    public void oneArgError() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action action = mock(Action.class);
+
+            Completable.error(new TestException())
+            .blockingSubscribe(action);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(action, never()).run();
+        });
+    }
+
+    @Test
+    public void oneArgErrorAsync() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action action = mock(Action.class);
+
+            Completable.error(new TestException())
+            .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe(action);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(action, never()).run();
+        });
+    }
+
+    @Test
+    public void twoArgComplete() throws Throwable {
+        Action action = mock(Action.class);
+        @SuppressWarnings("unchecked")
+        Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+        Completable.complete()
+        .blockingSubscribe(action, consumer);
+
+        verify(action).run();
+        verify(consumer, never()).accept(any());
+    }
+
+    @Test
+    public void twoArgCompleteAsync() throws Throwable {
+        Action action = mock(Action.class);
+        @SuppressWarnings("unchecked")
+        Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+        Completable.complete()
+        .delay(50, TimeUnit.MILLISECONDS)
+        .blockingSubscribe(action, consumer);
+
+        verify(action).run();
+        verify(consumer, never()).accept(any());
+    }
+
+    @Test
+    public void twoArgCompleteFails() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action action = mock(Action.class);
+            doThrow(new TestException()).when(action).run();
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Completable.complete()
+            .blockingSubscribe(action, consumer);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(action).run();
+            verify(consumer, never()).accept(any());
+        });
+    }
+
+    @Test
+    public void twoArgError() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action action = mock(Action.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Completable.error(new TestException())
+            .blockingSubscribe(action, consumer);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(action, never()).run();
+            verify(consumer).accept(any(TestException.class));
+        });
+    }
+
+    @Test
+    public void twoArgErrorAsync() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action action = mock(Action.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Completable.error(new TestException())
+            .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe(action, consumer);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(action, never()).run();
+            verify(consumer).accept(any(TestException.class));
+        });
+    }
+
+    @Test
+    public void twoArgErrorFails() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action action = mock(Action.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+            doThrow(new TestException()).when(consumer).accept(any());
+
+            Completable.error(new TestException())
+            .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe(action, consumer);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(action, never()).run();
+            verify(consumer).accept(any(TestException.class));
+        });
+    }
+
+    @Test
+    public void twoArgInterrupted() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action onDispose = mock(Action.class);
+
+            Action action = mock(Action.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Thread.currentThread().interrupt();
+
+            Completable.never()
+            .doOnDispose(onDispose)
+            .blockingSubscribe(action, consumer);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(onDispose).run();
+            verify(action, never()).run();
+            verify(consumer).accept(any(InterruptedException.class));
+        });
+    }
+
+    @Test
+    public void observerComplete() {
+        TestObserver<Void> to = new TestObserver<>();
+
+        Completable.complete()
+        .blockingSubscribe(to);
+
+        to.assertResult();
+    }
+
+    @Test
+    public void observerCompleteAsync() {
+        TestObserver<Void> to = new TestObserver<>();
+
+        Completable.complete()
+        .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+        .blockingSubscribe(to);
+
+        to.assertResult();
+    }
+
+    @Test
+    public void observerError() {
+        TestObserver<Void> to = new TestObserver<>();
+
+        Completable.error(new TestException())
+        .blockingSubscribe(to);
+
+        to.assertFailure(TestException.class);
+    }
+
+    @Test
+    public void observerErrorAsync() {
+        TestObserver<Void> to = new TestObserver<>();
+
+        Completable.error(new TestException())
+        .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+        .blockingSubscribe(to);
+
+        to.assertFailure(TestException.class);
+    }
+
+    @Test
+    public void observerDispose() throws Throwable {
+        Action onDispose = mock(Action.class);
+
+        TestObserver<Void> to = new TestObserver<>();
+        to.dispose();
+
+        Completable.never()
+        .doOnDispose(onDispose)
+        .blockingSubscribe(to);
+
+        to.assertEmpty();
+
+        verify(onDispose).run();
+    }
+
+    @Test
+    public void ovserverInterrupted() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action onDispose = mock(Action.class);
+
+            TestObserver<Void> to = new TestObserver<>();
+
+            Thread.currentThread().interrupt();
+
+            Completable.never()
+            .doOnDispose(onDispose)
+            .blockingSubscribe(to);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(onDispose).run();
+            to.assertFailure(InterruptedException.class);
+        });
+    }
+}
---
+++ src/test/java/io/reactivex/rxjava3/internal/operators/single/SingleBlockingSubscribeTest.java
@@ -0,0 +1,343 @@
+/**
+ * Copyright (c) 2016-present, RxJava Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.rxjava3.internal.operators.single;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.exceptions.TestException;
+import io.reactivex.rxjava3.functions.*;
+import io.reactivex.rxjava3.observers.TestObserver;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.testsupport.TestHelper;
+
+public class SingleBlockingSubscribeTest {
+
+    @Test
+    public void noArgSuccess() {
+        Single.just(1)
+        .blockingSubscribe();
+    }
+
+    @Test
+    public void noArgSuccessAsync() {
+        Single.just(1)
+        .delay(100, TimeUnit.MILLISECONDS)
+        .blockingSubscribe();
+    }
+
+    @Test
+    public void noArgError() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Single.error(new TestException())
+            .blockingSubscribe();
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+        });
+    }
+
+    @Test
+    public void noArgErrorAsync() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Single.error(new TestException())
+            .delay(100, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe();
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+        });
+    }
+
+    @Test
+    public void oneArgSuccess() throws Throwable {
+        @SuppressWarnings("unchecked")
+        Consumer<Integer> success = mock(Consumer.class);
+
+        Single.just(1)
+        .blockingSubscribe(success);
+
+        verify(success).accept(1);
+    }
+
+    @Test
+    public void oneArgSuccessAsync() throws Throwable {
+        @SuppressWarnings("unchecked")
+        Consumer<Integer> success = mock(Consumer.class);
+
+        Single.just(1)
+        .delay(50, TimeUnit.MILLISECONDS)
+        .blockingSubscribe(success);
+
+        verify(success).accept(1);
+    }
+
+    @Test
+    public void oneArgSuccessFails() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+            doThrow(new TestException()).when(success).accept(any());
+
+            Single.just(1)
+            .blockingSubscribe(success);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(success).accept(1);
+        });
+    }
+
+    @Test
+    public void oneArgError() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+
+            Single.<Integer>error(new TestException())
+            .blockingSubscribe(success);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(success, never()).accept(any());
+        });
+    }
+
+    @Test
+    public void oneArgErrorAsync() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+
+            Single.<Integer>error(new TestException())
+            .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe(success);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(success, never()).accept(any());
+        });
+    }
+
+    @Test
+    public void twoArgSuccess() throws Throwable {
+        @SuppressWarnings("unchecked")
+        Consumer<Integer> success = mock(Consumer.class);
+        @SuppressWarnings("unchecked")
+        Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+        Single.just(1)
+        .blockingSubscribe(success, consumer);
+
+        verify(success).accept(1);
+        verify(consumer, never()).accept(any());
+    }
+
+    @Test
+    public void twoArgSuccessAsync() throws Throwable {
+        @SuppressWarnings("unchecked")
+        Consumer<Integer> success = mock(Consumer.class);
+        @SuppressWarnings("unchecked")
+        Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+        Single.just(1)
+        .delay(50, TimeUnit.MILLISECONDS)
+        .blockingSubscribe(success, consumer);
+
+        verify(success).accept(any());
+        verify(consumer, never()).accept(any());
+    }
+
+    @Test
+    public void twoArgSuccessFails() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+            doThrow(new TestException()).when(success).accept(any());
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Single.just(1)
+            .blockingSubscribe(success, consumer);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(success).accept(any());
+            verify(consumer, never()).accept(any());
+        });
+    }
+
+    @Test
+    public void twoArgError() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Single.<Integer>error(new TestException())
+            .blockingSubscribe(success, consumer);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(success, never()).accept(any());
+            verify(consumer).accept(any(TestException.class));
+        });
+    }
+
+    @Test
+    public void twoArgErrorAsync() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Single.<Integer>error(new TestException())
+            .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe(success, consumer);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(success, never()).accept(any());
+            verify(consumer).accept(any(TestException.class));
+        });
+    }
+
+    @Test
+    public void twoArgErrorFails() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+            doThrow(new TestException()).when(consumer).accept(any());
+
+            Single.<Integer>error(new TestException())
+            .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+            .blockingSubscribe(success, consumer);
+
+            TestHelper.assertUndeliverable(errors, 0, TestException.class);
+
+            verify(success, never()).accept(any());
+            verify(consumer).accept(any(TestException.class));
+        });
+    }
+
+    @Test
+    public void twoArgInterrupted() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action onDispose = mock(Action.class);
+
+            @SuppressWarnings("unchecked")
+            Consumer<Integer> success = mock(Consumer.class);
+            @SuppressWarnings("unchecked")
+            Consumer<? super Throwable> consumer = mock(Consumer.class);
+
+            Thread.currentThread().interrupt();
+
+            Single.<Integer>never()
+            .doOnDispose(onDispose)
+            .blockingSubscribe(success, consumer);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(onDispose).run();
+            verify(success, never()).accept(any());
+            verify(consumer).accept(any(InterruptedException.class));
+        });
+    }
+
+    @Test
+    public void observerSuccess() {
+        TestObserver<Integer> to = new TestObserver<>();
+
+        Single.just(1)
+        .blockingSubscribe(to);
+
+        to.assertResult(1);
+    }
+
+    @Test
+    public void observerSuccessAsync() {
+        TestObserver<Integer> to = new TestObserver<>();
+
+        Single.just(1)
+        .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+        .blockingSubscribe(to);
+
+        to.assertResult(1);
+    }
+
+    @Test
+    public void observerError() {
+        TestObserver<Object> to = new TestObserver<>();
+
+        Single.error(new TestException())
+        .blockingSubscribe(to);
+
+        to.assertFailure(TestException.class);
+    }
+
+    @Test
+    public void observerErrorAsync() {
+        TestObserver<Object> to = new TestObserver<>();
+
+        Single.error(new TestException())
+        .delay(50, TimeUnit.MILLISECONDS, Schedulers.computation(), true)
+        .blockingSubscribe(to);
+
+        to.assertFailure(TestException.class);
+    }
+
+    @Test
+    public void observerDispose() throws Throwable {
+        Action onDispose = mock(Action.class);
+
+        TestObserver<Object> to = new TestObserver<>();
+        to.dispose();
+
+        Single.never()
+        .doOnDispose(onDispose)
+        .blockingSubscribe(to);
+
+        to.assertEmpty();
+
+        verify(onDispose).run();
+    }
+
+    @Test
+    public void ovserverInterrupted() throws Throwable {
+        TestHelper.withErrorTracking(errors -> {
+            Action onDispose = mock(Action.class);
+
+            TestObserver<Object> to = new TestObserver<>();
+
+            Thread.currentThread().interrupt();
+
+            Single.never()
+            .doOnDispose(onDispose)
+            .blockingSubscribe(to);
+
+            assertTrue("" + errors, errors.isEmpty());
+
+            verify(onDispose).run();
+            to.assertFailure(InterruptedException.class);
+        });
+    }
+}
