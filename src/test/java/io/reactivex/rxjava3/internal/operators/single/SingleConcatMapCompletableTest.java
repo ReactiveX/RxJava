57bd1a9272464a1b4b201faa2bab7a929a0ae7f3
David Karnok <akarnokd@gmail.com>
2020-01-26 20:13:17 +0100

    3.x: Add concatMapX operators (aliases) (#6879)

---
+++ src/test/java/io/reactivex/rxjava3/internal/operators/single/SingleConcatMapCompletableTest.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2016-present, RxJava Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.rxjava3.internal.operators.single;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import io.reactivex.rxjava3.core.*;
+import io.reactivex.rxjava3.exceptions.TestException;
+import io.reactivex.rxjava3.functions.*;
+import io.reactivex.rxjava3.testsupport.TestHelper;
+
+public class SingleConcatMapCompletableTest extends RxJavaTest {
+
+    @Test
+    public void dispose() {
+        TestHelper.checkDisposed(Single.just(1).concatMapCompletable(new Function<Integer, Completable>() {
+            @Override
+            public Completable apply(Integer v) throws Exception {
+                return Completable.complete();
+            }
+        }));
+    }
+
+    @Test
+    public void normal() {
+        final boolean[] b = { false };
+
+        Single.just(1)
+        .concatMapCompletable(new Function<Integer, Completable>() {
+            @Override
+            public Completable apply(Integer t) throws Exception {
+                return Completable.complete().doOnComplete(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        b[0] = true;
+                    }
+                });
+            }
+        })
+        .test()
+        .assertResult();
+
+        assertTrue(b[0]);
+    }
+
+    @Test
+    public void error() {
+        final boolean[] b = { false };
+
+        Single.<Integer>error(new TestException())
+        .concatMapCompletable(new Function<Integer, Completable>() {
+            @Override
+            public Completable apply(Integer t) throws Exception {
+                return Completable.complete().doOnComplete(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        b[0] = true;
+                    }
+                });
+            }
+        })
+        .test()
+        .assertFailure(TestException.class);
+
+        assertFalse(b[0]);
+    }
+
+    @Test
+    public void mapperThrows() {
+        final boolean[] b = { false };
+
+        Single.just(1)
+        .concatMapCompletable(new Function<Integer, Completable>() {
+            @Override
+            public Completable apply(Integer t) throws Exception {
+                throw new TestException();
+            }
+        })
+        .test()
+        .assertFailure(TestException.class);
+
+        assertFalse(b[0]);
+    }
+
+    @Test
+    public void mapperReturnsNull() {
+        final boolean[] b = { false };
+
+        Single.just(1)
+        .concatMapCompletable(new Function<Integer, Completable>() {
+            @Override
+            public Completable apply(Integer t) throws Exception {
+                return null;
+            }
+        })
+        .test()
+        .assertFailure(NullPointerException.class);
+
+        assertFalse(b[0]);
+    }
+}
---
+++ src/test/java/io/reactivex/rxjava3/internal/operators/single/SingleConcatMapTest.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2016-present, RxJava Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+package io.reactivex.rxjava3.internal.operators.single;
+
+import org.junit.Test;
+
+import io.reactivex.rxjava3.core.*;
+import io.reactivex.rxjava3.exceptions.TestException;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.testsupport.TestHelper;
+
+public class SingleConcatMapTest extends RxJavaTest {
+
+    @Test
+    public void concatMapValue() {
+        Single.just(1).concatMap(new Function<Integer, SingleSource<Integer>>() {
+            @Override public SingleSource<Integer> apply(final Integer integer) throws Exception {
+                if (integer == 1) {
+                    return Single.just(2);
+                }
+
+                return Single.just(1);
+            }
+        })
+            .test()
+            .assertResult(2);
+    }
+
+    @Test
+    public void concatMapValueDifferentType() {
+        Single.just(1).concatMap(new Function<Integer, SingleSource<String>>() {
+            @Override public SingleSource<String> apply(final Integer integer) throws Exception {
+                if (integer == 1) {
+                    return Single.just("2");
+                }
+
+                return Single.just("1");
+            }
+        })
+            .test()
+            .assertResult("2");
+    }
+
+    @Test
+    public void concatMapValueNull() {
+        Single.just(1).concatMap(new Function<Integer, SingleSource<Integer>>() {
+            @Override public SingleSource<Integer> apply(final Integer integer) throws Exception {
+                return null;
+            }
+        })
+        .to(TestHelper.<Integer>testConsumer())
+            .assertNoValues()
+            .assertError(NullPointerException.class)
+            .assertErrorMessage("The single returned by the mapper is null");
+    }
+
+    @Test
+    public void concatMapValueErrorThrown() {
+        Single.just(1).concatMap(new Function<Integer, SingleSource<Integer>>() {
+            @Override public SingleSource<Integer> apply(final Integer integer) throws Exception {
+                throw new RuntimeException("something went terribly wrong!");
+            }
+        })
+            .to(TestHelper.<Integer>testConsumer())
+            .assertNoValues()
+            .assertError(RuntimeException.class)
+            .assertErrorMessage("something went terribly wrong!");
+    }
+
+    @Test
+    public void concatMapError() {
+        RuntimeException exception = new RuntimeException("test");
+
+        Single.error(exception).concatMap(new Function<Object, SingleSource<Object>>() {
+            @Override public SingleSource<Object> apply(final Object integer) throws Exception {
+                return Single.just(new Object());
+            }
+        })
+            .test()
+            .assertError(exception);
+    }
+
+    @Test
+    public void dispose() {
+        TestHelper.checkDisposed(Single.just(1).concatMap(new Function<Integer, SingleSource<Integer>>() {
+            @Override
+            public SingleSource<Integer> apply(Integer v) throws Exception {
+                return Single.just(2);
+            }
+        }));
+    }
+
+    @Test
+    public void mappedSingleOnError() {
+        Single.just(1).concatMap(new Function<Integer, SingleSource<Integer>>() {
+            @Override
+            public SingleSource<Integer> apply(Integer v) throws Exception {
+                return Single.error(new TestException());
+            }
+        })
+        .test()
+        .assertFailure(TestException.class);
+    }
+
+    @Test
+    public void doubleOnSubscribe() {
+        TestHelper.checkDoubleOnSubscribeSingle(new Function<Single<Object>, SingleSource<Object>>() {
+            @Override
+            public SingleSource<Object> apply(Single<Object> s)
+                    throws Exception {
+                return s.concatMap(new Function<Object, SingleSource<? extends Object>>() {
+                    @Override
+                    public SingleSource<? extends Object> apply(Object v)
+                            throws Exception {
+                        return Single.just(v);
+                    }
+                });
+            }
+        });
+    }
+}
