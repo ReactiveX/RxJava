bfd242da5de53bd16f8c7d52c83e0968010bba2c
David Karnok <akarnokd@gmail.com>
2020-11-30 20:21:14 +0100

    3.x: Remove unnecessary cancel/dispose calls from terminating using (#7121)

--- src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableUsing.java
+++ src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableUsing.java
@@ -115,11 +115,9 @@ public final class ObservableUsing<T, D> extends Observable<T> {
                     }
                 }
 
-                upstream.dispose();
                 downstream.onError(t);
             } else {
                 downstream.onError(t);
-                upstream.dispose();
                 disposeResource();
             }
         }
@@ -137,11 +135,9 @@ public final class ObservableUsing<T, D> extends Observable<T> {
                     }
                 }
 
-                upstream.dispose();
                 downstream.onComplete();
             } else {
                 downstream.onComplete();
-                upstream.dispose();
                 disposeResource();
             }
         }
