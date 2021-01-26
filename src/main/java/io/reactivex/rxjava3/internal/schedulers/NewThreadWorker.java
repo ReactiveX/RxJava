80a4842a7603da2d1c96fc2bb16249605cde5bbd
Archish Thakkar <archishthakkar@gmail.com>
2021-01-26 22:53:17 +0530

    Updating documentation for scheduleActual method (#7164)

--- src/main/java/io/reactivex/rxjava3/internal/schedulers/NewThreadWorker.java
+++ src/main/java/io/reactivex/rxjava3/internal/schedulers/NewThreadWorker.java
@@ -116,10 +116,8 @@ public class NewThreadWorker extends Scheduler.Worker implements Disposable {
     }
 
     /**
-     * Wraps the given runnable into a ScheduledRunnable and schedules it
+     * Wraps and returns the given runnable into a ScheduledRunnable and schedules it
      * on the underlying ScheduledExecutorService.
-     * <p>If the schedule has been rejected, the ScheduledRunnable.wasScheduled will return
-     * false.
      * @param run the runnable instance
      * @param delayTime the time to delay the execution
      * @param unit the time unit
