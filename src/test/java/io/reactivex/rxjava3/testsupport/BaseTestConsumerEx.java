c2bf5148d8cb094a984677813c000c762f0daf50
Jordan Gout <5838406+keytronic@users.noreply.github.com>
2021-10-14 19:30:35 +0200

    Fix assert message format (#7351)
    * Fix assert message format
    
    * Test error messages against regex

--- src/test/java/io/reactivex/rxjava3/testsupport/BaseTestConsumerEx.java
+++ src/test/java/io/reactivex/rxjava3/testsupport/BaseTestConsumerEx.java
@@ -160,7 +160,8 @@ extends BaseTestConsumer<T, U> {
             Throwable e = errors.get(0);
             String errorMessage = e.getMessage();
             if (!Objects.equals(message, errorMessage)) {
-                throw fail("Error message differs;\nexpected: " + message + "\ngot: " + errorMessage);
+                throw fail("\nexpected: " + message + "\ngot: " + errorMessage
+                + "; Error message differs");
             }
         } else {
             throw fail("Multiple errors");
