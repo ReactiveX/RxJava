76340d91be2ac0c8028321afdbdc28cb4050c187
Chris Povirk <cpovirk@google.com>
2023-08-21 11:04:40 -0400

    Remove `@NonNull` annotation from `Collector` container type parameter.
    `collect` delegates to `FlowableCollectWithCollectorSingle`, which does
    is not annotated to require a non-nullable type for its corresponding
    type parameter and whose implementation works fine with a null
    container.
    
    This PR lets Kotlin code pass a `Collector<T, A, R>`, where `A` is
    declared as `<A: Any?>`.

--- src/main/java/io/reactivex/rxjava3/core/Flowable.java
+++ src/main/java/io/reactivex/rxjava3/core/Flowable.java
@@ -20394,7 +20394,7 @@ public abstract class Flowable<@NonNull T> implements Publisher<T> {
     @BackpressureSupport(BackpressureKind.UNBOUNDED_IN)
     @SchedulerSupport(SchedulerSupport.NONE)
     @NonNull
-    public final <@NonNull R, @NonNull A> Single<R> collect(@NonNull Collector<? super T, A, R> collector) {
+    public final <@NonNull R, @Nullable A> Single<R> collect(@NonNull Collector<? super T, A, R> collector) {
         Objects.requireNonNull(collector, "collector is null");
         return RxJavaPlugins.onAssembly(new FlowableCollectWithCollectorSingle<>(this, collector));
     }
